fb7891bfad563d106d9368b3388113a75b58a565
ngoodman@bayontechnologies.com <ngoodman@bayontechnologies.com@66c6c761-dd4f-0410-b885-6f950aad2415>
2010-03-18 03:30:24 +0000

    [CDF-89] - added required imports, duh!

--- bi-platform-v2-plugin/src/org/pentaho/cdf/CdfContentGenerator.java
+++ bi-platform-v2-plugin/src/org/pentaho/cdf/CdfContentGenerator.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.security.InvalidParameterException;
 import java.util.Date;
 import java.util.Enumeration;
