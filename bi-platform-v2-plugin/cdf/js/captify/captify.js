da6b7fa255f84caa79d80de3d32a01d43974392e
pmgalves <pmgalves@66c6c761-dd4f-0410-b885-6f950aad2415>
2010-04-21 11:31:45 +0000

    - Fixed button component samples; Added mising fancybox resources for IE

--- bi-platform-v2-plugin/cdf/js/captify/captify.js
+++ bi-platform-v2-plugin/cdf/js/captify/captify.js
@@ -1,35 +1,28 @@
-/* jQuery Captify Plugin
-* 
-* Copyright (C) 2008 Brian Reavis
-* Licenced under the MIT license
-*
-* $Date: 2008-2-27 [Fri, 27 Feb 2009] $
-*/
-
+// jQuery Captify Plugin
+// Copyright (C) 2009 Brian Reavis
+// Licenced under the MIT license
+// $Date: 2009-11-30 [Mon, 30 Nov 2009] $
 jQuery.fn.extend({
-	captify: function(o,object) {
+	captify: function(uo) {
 		var o = $.extend({
 			speedOver: 'fast',				// speed of the mouseover effect
 			speedOut: 'normal',				// speed of the mouseout effect
-			hideDelay: 4000,					// how long to delay the hiding of the caption after mouseout (ms)
+			hideDelay: 500,					// how long to delay the hiding of the caption after mouseout (ms)
 			animation: 'slide',				// 'fade', 'slide', 'always-on'
 			prefix: '',						// text/html to be placed at the beginning of every caption
-			opacity: '0.35',				// opacity of the caption on mouse over
+			opacity: '0.45',				// opacity of the caption on mouse over
 			className: 'caption-bottom',	// the name of the CSS class to apply to the caption box         
 			position: 'bottom',				// position of the caption (top or bottom)         
 			spanWidth: '100%'				// caption span % of the image
-		}, o);
+		}, uo);
 		$(this).each(function() {
 			var img = this;
 			$(this).load(function() {
-				$this = img;
-				if (this.hasInit) {
-					return false;
-				}
-				this.hasInit = true;
-				var close = 0;
-				var invoker;
-			
+				if (img.hasInit){ return false; }
+				img.hasInit = true;
+				var over_caption = false;
+				var over_img = false;
+
 				//pull the label from another element if there is a 
 				//valid element id inside the rel="..." attribute, otherwise,
 				//just use the text in the alt="..." attribute.
@@ -37,15 +30,17 @@ jQuery.fn.extend({
 				var captionLabelHTML = !captionLabelSrc.length ? $(this).attr('alt') : captionLabelSrc.html();
 				captionLabelSrc.remove();
 				var toWrap = this.parent && this.parent.tagName == 'a' ? this.parent : $(this);
-				var wrapper = toWrap.wrap('<div></div>').parent();
-				wrapper.css({
-					overflow: 'hidden',
-					padding: 0,
-					fontSize: 0.1
-				})
-				wrapper.addClass('caption-wrapper');
-				wrapper.width($(this).width());
-				wrapper.height($(this).height());
+				
+				var wrapper = 
+					toWrap.wrap('<div></div>').parent()
+					.css({
+						overflow: 'hidden',
+						padding: 0,
+						fontSize: 0.1
+					})
+					.addClass('caption-wrapper')
+					.width($(this).width())
+					.height($(this).height());
 
 				//transfer the margin and border properties from the image to the wrapper
 				$.map(['top', 'right', 'bottom', 'left'], function(i) {
@@ -59,11 +54,14 @@ jQuery.fn.extend({
 
 				//create two consecutive divs, one for the semi-transparent background,
 				//and other other for the fully-opaque label
-				var button = $('<div></div>').addClass('caption-button');
-				if(o.hasButton){toWrap.parent().parent().prepend(button);}
-				var caption = $('div:last', wrapper.append('<div></div>')).addClass(o.className);
-				var captionContent = $('div:last', wrapper.append('<div></div>')).addClass(o.className).append(o.prefix).append(captionLabelHTML);
-				
+				var caption = $('div:last', wrapper.append('<div></div>'))
+					.addClass(o.className);
+					
+				var captionContent = $('div:last', wrapper.append('<div></div>'))
+					.addClass(o.className)
+					.append(o.prefix)
+					.append(captionLabelHTML);
+
 				//override hiding from CSS, and reset all margins (which could have been inherited)
 				$('*', wrapper).css({ margin: 0 }).show();
 
@@ -77,7 +75,9 @@ jQuery.fn.extend({
 				});
 
 				if (o.position == 'bottom'){
-					var vLabelOffset = parseInt(caption.css('border-top-width').replace('px', '')) + parseInt(captionContent.css('padding-top').replace('px', '')) - 1;
+					var vLabelOffset = 
+						parseInt(caption.css('border-top-width').replace('px', '')) + 
+						parseInt(captionContent.css('padding-top').replace('px', '')) - 1;
 					captionContent.css('paddingTop', vLabelOffset);
 				}
 				//clear the backgrounds/borders from the label, and make it fully-opaque
@@ -85,18 +85,16 @@ jQuery.fn.extend({
 					position: captionPositioning,
 					zIndex: 2,
 					background: 'none',
-					border: '1 none',
-					opacity: o.animation == 'fade' ? 1 : 1,
+					border: '0 none',
+					opacity: o.animation == 'fade' ? 0 : 1,
 					width: o.spanWidth
 				});
 				caption.width(captionContent.outerWidth());
 				caption.height(captionContent.height());
-				button.width($(img).outerWidth());
-				
 
 				// represents caption margin positioning for hide and show states
-				var topBorderAdj = (o.position == 'bottom' && jQuery.browser.msie) ? -4 : 0;
-				var captionPosition = (o.position == 'top')
+				var topBorderAdj = o.position == 'bottom' && jQuery.browser.msie ? -4 : 0;
+				var captionPosition = o.position == 'top'
 				   ? { hide: -$(img).height() - caption.outerHeight() - 1, show: -$(img).height() }
 				   : { hide: 0, show: -caption.outerHeight() + topBorderAdj };
 				
@@ -106,47 +104,48 @@ jQuery.fn.extend({
 				
 				//function to push the caption out of view
 				var cHide = function() {
-					if (close){
+					if (!over_caption && !over_img){
 						var props = o.animation == 'fade'
 							? { opacity: 0 }
 							: { marginTop: captionPosition.hide };
-						caption.animate(props, 250,null,function(){close = false});
-						invoker.animate(props, 250);
+						caption.animate(props, o.speedOut);
 						if (o.animation == 'fade'){
-							captionContent.animate({opacity: 0}, o.speedOver,null,function(){close = false});
+							captionContent.animate({opacity: 0}, o.speedOver);
 						}
-						
 					}
-					
 				};
-				
-				
 
 				if (o.animation != 'always-on'){
 					//when the mouse is over the image
-					//$(this).hover(
-					$(this).bind("detailsClick",
-						function(e,obj) {
-							if (!close) {
-								close = true;
-								invoker = $(obj);
+					$(this).hover(
+						function() {
+							over_img = true;
+							if (!over_caption) {
 								var props = o.animation == 'fade'
 									? { opacity: o.opacity }
 									: { marginTop: captionPosition.show };
 								caption.animate(props, o.speedOver);
-								invoker.animate(props, o.speedOver);
 								if (o.animation == 'fade'){
 									captionContent.animate({opacity: 1}, o.speedOver/2);
 								}
-								
-								window.setTimeout(cHide, o.hideDelay);
 							}
-						});
+						},
+						function() {
+							over_img = false;
+							window.setTimeout(cHide, o.hideDelay);
+						}
+					);
+					//when the mouse is over the caption on top of the image (the caption is a sibling of the image)
+					$('div', wrapper).hover(
+						function() { over_caption = true; },
+						function() { over_caption = false; window.setTimeout(cHide, o.hideDelay); }
+					);
 				}
-				
-				o.bDetails.trigger('capityFinished',[this]);
-				
 			});
+			//if the image has already loaded (due to being cached), force the load function to be called
+			if (this.complete || this.naturalWidth > 0) {
+				$(img).trigger('load');
+			}
 		});
 	}
 });
