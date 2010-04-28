5c94d40def1559148db29e72c9b4a6deb80dc3cc
paula.clemente <paula.clemente@66c6c761-dd4f-0410-b885-6f950aad2415>
2010-04-28 15:49:11 +0000

    added dataTable fnLengthChange function to CoreComponents.js

--- bi-platform-v2-plugin/cdf/js/CoreComponents.js
+++ bi-platform-v2-plugin/cdf/js/CoreComponents.js
@@ -1058,6 +1058,45 @@ var JpivotComponent = BaseComponent.extend({
 		}
 	});
 
+
+/*
+ * Function: fnLengthChange
+ * Purpose:  Change the number of records on display
+ * Returns:  array:
+ * Inputs:   object:oSettings - DataTables settings object
+ *           int:iDisplay - New display length
+ */
+$.fn.dataTableExt.oApi.fnLengthChange = function ( oSettings, iDisplay )
+{
+    oSettings._iDisplayLength = iDisplay;
+    oSettings.oApi._fnCalculateEnd( oSettings );
+     
+    /* If we have space to show extra rows (backing up from the end point - then do so */
+    if ( oSettings._iDisplayEnd == oSettings.aiDisplay.length )
+    {
+        oSettings._iDisplayStart = oSettings._iDisplayEnd - oSettings._iDisplayLength;
+        if ( oSettings._iDisplayStart < 0 )
+        {
+            oSettings._iDisplayStart = 0;
+        }
+    }
+     
+    if ( oSettings._iDisplayLength == -1 )
+    {
+        oSettings._iDisplayStart = 0;
+    }
+     
+    oSettings.oApi._fnDraw( oSettings );
+     
+    $('select', oSettings.oFeatures.l).val( iDisplay );
+};
+/* Example 
+ * $(document).ready(function() {
+ *    var oTable = $('#example').dataTable();
+ *    oTable.fnLengthChange( 100 );
+ * } );
+ */
+
 var TableComponent = BaseComponent.extend({
 		update : function() {
 			var cd = this.chartDefinition;
