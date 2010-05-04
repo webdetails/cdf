ad8b3e42dbee65167402a1c998e64276afa4aa47
pmgalves <pmgalves@66c6c761-dd4f-0410-b885-6f950aad2415>
2010-05-04 12:13:36 +0000

    - CoreComponents no longer requires datatables to be read before

--- bi-platform-v2-plugin/cdf/js/CoreComponents.js
+++ bi-platform-v2-plugin/cdf/js/CoreComponents.js
@@ -1066,36 +1066,38 @@ var JpivotComponent = BaseComponent.extend({
  * Inputs:   object:oSettings - DataTables settings object
  *           int:iDisplay - New display length
  */
-$.fn.dataTableExt.oApi.fnLengthChange = function ( oSettings, iDisplay )
-{
-    oSettings._iDisplayLength = iDisplay;
-    oSettings.oApi._fnCalculateEnd( oSettings );
-     
-    /* If we have space to show extra rows (backing up from the end point - then do so */
-    if ( oSettings._iDisplayEnd == oSettings.aiDisplay.length )
-    {
-        oSettings._iDisplayStart = oSettings._iDisplayEnd - oSettings._iDisplayLength;
-        if ( oSettings._iDisplayStart < 0 )
-        {
-            oSettings._iDisplayStart = 0;
-        }
-    }
-     
-    if ( oSettings._iDisplayLength == -1 )
-    {
-        oSettings._iDisplayStart = 0;
-    }
-     
-    oSettings.oApi._fnDraw( oSettings );
-     
-    $('select', oSettings.oFeatures.l).val( iDisplay );
-};
-/* Example 
- * $(document).ready(function() {
- *    var oTable = $('#example').dataTable();
- *    oTable.fnLengthChange( 100 );
- * } );
- */
+if(typeof $.fn.dataTablesExt != undefined){ // Ensure we load dataTables before this line. If not, just keep going
+	$.fn.dataTableExt.oApi.fnLengthChange = function ( oSettings, iDisplay )
+	{
+		oSettings._iDisplayLength = iDisplay;
+		oSettings.oApi._fnCalculateEnd( oSettings );
+
+		// If we have space to show extra rows backing up from the end point - then do so 
+		if ( oSettings._iDisplayEnd == oSettings.aiDisplay.length )
+		{
+			oSettings._iDisplayStart = oSettings._iDisplayEnd - oSettings._iDisplayLength;
+			if ( oSettings._iDisplayStart < 0 )
+			{
+				oSettings._iDisplayStart = 0;
+			}
+		}
+
+		if ( oSettings._iDisplayLength == -1 )
+		{
+			oSettings._iDisplayStart = 0;
+		}
+
+		oSettings.oApi._fnDraw( oSettings );
+
+		$('select', oSettings.oFeatures.l).val( iDisplay );
+	};
+	/* Example 
+	 * $(document).ready(function() {
+	 *    var oTable = $('#example').dataTable();
+	 *    oTable.fnLengthChange( 100 );
+	 * } );
+	 */
+}
 
 var TableComponent = BaseComponent.extend({
 		update : function() {
