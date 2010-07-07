e8648bd9fb9f2b174b5ea6edf6d60fa3c5fc5298
sramazzina@gmail.com <sramazzina@gmail.com@66c6c761-dd4f-0410-b885-6f950aad2415>
2010-07-07 16:01:40 +0000

    Managed global and dashboard specific message files. Added "messages" element to xcdf descriptor to enable per dashboard localization. Various refactorings to start cleaning the code.

---
+++ bi-platform-v2-plugin/src/org/pentaho/cdf/CdfConstants.java
@@ -0,0 +1,16 @@
+package org.pentaho.cdf;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: sramazzina
+ * Date: 7-lug-2010
+ * Time: 11.30.59
+ * To change this template use File | Settings | File Templates.
+ */
+public class CdfConstants {
+
+    // CDF localization related constants
+    public static final String BASE_GLOBAL_MESSAGE_SET_FILENAME = "Messages";
+    public static final String BASE_CDF_CACHE_DIR = "tmp/.cache";
+    public static final String BASE_GLOBAL_MESSAGE_SET_URL = "resources/languages/";
+}
---
+++ bi-platform-v2-plugin/src/org/pentaho/cdf/localization/MessageBundlesHelper.java
@@ -0,0 +1,122 @@
+package org.pentaho.cdf.localization;
+
+import org.pentaho.cdf.CdfConstants;
+import org.pentaho.platform.engine.core.solution.ActionInfo;
+import org.pentaho.platform.engine.core.system.PentahoSystem;
+import org.pentaho.platform.util.messages.LocaleHelper;
+
+import java.io.*;
+import java.util.Locale;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: sramazzina
+ * Date: 7-lug-2010
+ * Time: 11.27.43
+ * To change this template use File | Settings | File Templates.
+ */
+public class MessageBundlesHelper {
+
+    private String baseUrl;
+    private String globalBaseMessageFile;
+    private String targetDashboardCacheDir;
+    private String targetDashboardBaseMsgFile;
+    private String sourceDashboardBaseMsgFile;
+    private static final String PENTAHO_CDF_GLOBAL_LANGUAGES_DIR = "pentaho-cdf/resources/languages";
+    private static final String PENTAHO_CDF_DIR = "pentaho-cdf/";
+    private String languagesCacheUrl;
+
+
+    public MessageBundlesHelper(String baseGlobalMessageSetFilename,
+                                String dashboardSolution,
+                                String dashboardPath,
+                                String dashboardsMessagesBaseFilename) {
+        init(baseGlobalMessageSetFilename,
+             dashboardSolution,
+             dashboardPath,
+             dashboardsMessagesBaseFilename);
+    }
+
+    public void saveI18NMessageFilesToCache() throws IOException {
+        createCacheDirIfNoExists(targetDashboardCacheDir);
+
+        appendBaseMessageFiles(sourceDashboardBaseMsgFile, globalBaseMessageFile, targetDashboardBaseMsgFile);
+        appendLocalizedMessageFiles(sourceDashboardBaseMsgFile, globalBaseMessageFile, targetDashboardBaseMsgFile);
+    }
+
+
+    public String getMessageFilesCacheUrl() {
+        return languagesCacheUrl.replace(File.separator, "/");
+    }
+
+    protected void init(String baseGlobalMessageSetFilename,
+                        String dashboardSolution,
+                        String dashboardPath,
+                        String dashboardsMessagesBaseFilename) {
+
+        baseUrl = PentahoSystem.getApplicationContext().getSolutionPath("");
+        globalBaseMessageFile = baseUrl + ActionInfo.buildSolutionPath("system", PENTAHO_CDF_GLOBAL_LANGUAGES_DIR, baseGlobalMessageSetFilename);
+        languagesCacheUrl = CdfConstants.BASE_CDF_CACHE_DIR + File.separator + dashboardSolution + dashboardPath;
+        targetDashboardCacheDir = baseUrl + File.separator + ActionInfo.buildSolutionPath("system", PENTAHO_CDF_DIR + languagesCacheUrl, "");
+        targetDashboardBaseMsgFile = baseUrl + File.separator + ActionInfo.buildSolutionPath("system", PENTAHO_CDF_DIR + languagesCacheUrl, dashboardsMessagesBaseFilename);
+        sourceDashboardBaseMsgFile = baseUrl + File.separator + ActionInfo.buildSolutionPath(dashboardSolution, dashboardPath, dashboardsMessagesBaseFilename);
+    }
+
+    protected void createCacheDirIfNoExists(String targetDashboardCacheDir) {
+        File fBaseMsgTargetDir = new File(targetDashboardCacheDir);
+        if (!fBaseMsgTargetDir.exists()) {
+            fBaseMsgTargetDir.mkdirs();
+        }
+    }
+
+    protected void appendBaseMessageFiles(String sourceDashboardBaseMsgFile,
+                                    String globalBaseMessageFile,
+                                    String targetDashboardBaseMsgFile) throws IOException {
+
+        appendMessageFiles(sourceDashboardBaseMsgFile, globalBaseMessageFile, targetDashboardBaseMsgFile);
+    }
+
+    protected void appendLocalizedMessageFiles(String sourceDashboardBaseMsgFile,
+                                    String globalBaseMessageFile,
+                                    String targetDashboardBaseMsgFile) throws IOException {
+
+        Locale locale = LocaleHelper.getLocale();
+        // Manage selected language file
+        sourceDashboardBaseMsgFile = sourceDashboardBaseMsgFile + "_" + locale.toString();
+        globalBaseMessageFile = globalBaseMessageFile + "_" + locale.toString();
+        targetDashboardBaseMsgFile = targetDashboardBaseMsgFile + "_" + locale.toString();
+
+        appendMessageFiles(sourceDashboardBaseMsgFile, globalBaseMessageFile, targetDashboardBaseMsgFile);
+    }
+
+
+    protected void appendMessageFiles(String sourceDashboardBaseMsgFile,
+                                    String globalBaseMessageFile,
+                                    String targetDashboardBaseMsgFile) throws IOException {
+
+        File fBaseMsgDashboard = new File(sourceDashboardBaseMsgFile + ".properties");
+        File fBaseMsgGlobal = new File(globalBaseMessageFile + ".properties");
+        File fBaseMsgTarget = new File(targetDashboardBaseMsgFile + ".properties");
+
+        String theLine;
+        if (!fBaseMsgTarget.exists()) {
+            fBaseMsgTarget.createNewFile();
+            BufferedWriter bwBaseMsgTarget = new BufferedWriter(new FileWriter(fBaseMsgTarget, true));
+            if (fBaseMsgGlobal.exists()) {
+                BufferedReader brBaseMsgGlobal = new BufferedReader(new FileReader(fBaseMsgGlobal));
+                while ((theLine = brBaseMsgGlobal.readLine()) != null) {
+                    bwBaseMsgTarget.write(theLine + "\n");
+                }
+                brBaseMsgGlobal.close();
+            }
+            if (fBaseMsgDashboard.exists()) {
+                BufferedReader brBaseMsgDashboard = new BufferedReader(new FileReader(fBaseMsgDashboard));
+                while ((theLine = brBaseMsgDashboard.readLine()) != null) {
+                    bwBaseMsgTarget.write(theLine + "\n");
+                }
+                brBaseMsgDashboard.close();
+            }
+            bwBaseMsgTarget.close();
+        }
+    }
+}
