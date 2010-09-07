c3cc7322f182559f823d857e145a5d36c2dd4eeb
adphillips <adphillips@66c6c761-dd4f-0410-b885-6f950aad2415>
2010-09-07 15:16:08 +0000

    [CDF-115] - fixed an XML parsing error that was preventing trunk CDF to work with Pentaho BIServer 3.6 or 3.7

--- bi-platform-v2-plugin/src/org/pentaho/cdf/CdfFileInfoGenerator.java
+++ bi-platform-v2-plugin/src/org/pentaho/cdf/CdfFileInfoGenerator.java
@@ -2,20 +2,26 @@ package org.pentaho.cdf;
 
 import java.io.InputStream;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.dom4j.Document;
 import org.pentaho.platform.api.engine.IFileInfo;
 import org.pentaho.platform.api.engine.ISolutionFile;
 import org.pentaho.platform.api.engine.SolutionFileMetaAdapter;
 import org.pentaho.platform.engine.core.solution.FileInfo;
-import org.pentaho.platform.util.xml.dom4j.XmlDom4JHelper;
+import org.w3c.dom.Document;
 
 /**
  * Parses a Dom4J document and creates an IFileInfo object containing the
  * xcdf info.
  * 
  * @author Will Gorman (wgorman@pentaho.com)
+ * @author Aaron Phillips (aphillips@pentaho.com) modified for latest plugin spec
  */
 public class CdfFileInfoGenerator extends SolutionFileMetaAdapter {
 
@@ -26,32 +32,31 @@ public class CdfFileInfoGenerator extends SolutionFileMetaAdapter {
 
   @Override
   public IFileInfo getFileInfo(ISolutionFile solutionFile, InputStream in) {
-    // TODO Auto-generated method stubT
     Document doc = null;
     try {
-      doc = XmlDom4JHelper.getDocFromStream(in);
+      DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+      DocumentBuilder builder = factory.newDocumentBuilder();
+      doc = builder.parse(in);
+      
+      String result = "dashboard";  //$NON-NLS-1$
+      
+      XPath xpath = XPathFactory.newInstance().newXPath();
+      String author = (String) xpath.evaluate("/cdf/author", doc, XPathConstants.STRING); //$NON-NLS-1$
+      String description = (String) xpath.evaluate("/cdf/description", doc, XPathConstants.STRING); //$NON-NLS-1$
+      String icon = (String) xpath.evaluate("/cdf/icon", doc, XPathConstants.STRING); //$NON-NLS-1$
+      String title = (String) xpath.evaluate("/cdf/title", doc, XPathConstants.STRING); //$NON-NLS-1$
+
+      IFileInfo info = new FileInfo();
+      info.setAuthor(author);
+      info.setDescription(description);
+      info.setDisplayType(result);
+      info.setIcon(icon);
+      info.setTitle(title);
+      return info;
+
     } catch (Exception e) {
       logger.error(Messages.getErrorString("CdfFileInfoGenerator.ERROR_0001_PARSING_XCDF"), e); //$NON-NLS-1$
       return null;
     }
-    if (doc == null) {
-      logger.error(Messages.getErrorString("CdfFileInfoGenerator.ERROR_0001_PARSING_XCDF")); //$NON-NLS-1$
-      return null;
-    }
-    
-    String result = "dashboard";  //$NON-NLS-1$
-
-    String author = XmlDom4JHelper.getNodeText("/cdf/author", doc, "");  //$NON-NLS-1$ //$NON-NLS-2$
-    String description = XmlDom4JHelper.getNodeText("/cdf/description", doc, "");  //$NON-NLS-1$ //$NON-NLS-2$
-    String icon = XmlDom4JHelper.getNodeText("/cdf/icon", doc, "");  //$NON-NLS-1$ //$NON-NLS-2$
-    String title = XmlDom4JHelper.getNodeText("/cdf/title", doc, "");  //$NON-NLS-1$ //$NON-NLS-2$
-
-    IFileInfo info = new FileInfo();
-    info.setAuthor(author);
-    info.setDescription(description);
-    info.setDisplayType(result);
-    info.setIcon(icon);
-    info.setTitle(title);
-    return info;
   }
 }
