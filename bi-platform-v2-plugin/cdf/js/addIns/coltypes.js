090bca4d8d440082bd09f8ef6f4e49f8ed2fdd70
Pedro Pinheiro <pdpinheiro@gmail.com>
2011-12-14 04:45:05 +0000

    Several colType changes
    - Add a goodness parameter to trendArrows to
    describe whether the measure is desirable (growing
    is good) or undesirable (dwindling is good)
    
    - Add a mechanism to provide both an url and its
    label in hyperlinks
    
    - Add a new circle colType rendering a disk with
    dynamic radius and/or color
    
    - Add a BulletChart colType based on CCC Bullet
    Charts

--- bi-platform-v2-plugin/cdf/js/addIns/coltypes.js
+++ bi-platform-v2-plugin/cdf/js/addIns/coltypes.js
@@ -170,6 +170,7 @@
     name: "trendArrow",
     label: "Trend Arrows",
     defaults: {
+      good: true,
       includeValue: false,
       valueFormat: function(v,format,st) {
         return sprintf(format || "%.1f",v);
@@ -181,10 +182,10 @@
     },
     implementation: function(tgt, st, opt) {
       var ph = $(tgt),
+      qualityClass = opt.good ? "good" : "bad",
       trendClass =  st.value < 0 ? "down" : "up";
       var trend = $("<div>&nbsp;</div>");
-      trend.addClass('trend');
-      trend.addClass(trendClass);
+      trend.addClass('trend ' + trendClass + ' '  + qualityClass);
       ph.empty();
       if(opt.includeValue) {
         ph.append(opt.valueFormat(st.value, st.colFormat, st));
@@ -201,7 +202,10 @@
     defaults:{
       openInNewTab: true,
       prependHttpIfNeeded: true,
-      regexp: null
+      regexp: null,
+      pattern: null,
+      urlReference: 2,
+      labelReference: 1
     },
     
     init: function(){
@@ -212,14 +216,23 @@
     implementation: function(tgt, st, opt){
       
       var ph = $(tgt);
-      var link = st.value;
+      var link, label;
+      if (opt.pattern) {
+        var re = new RegExp(opt.pattern),
+          results = re.exec(st.value);
+        link = results[opt.urlReference];
+        label = results[opt.labelReference];
+      } else {
+        link = st.value;
+        label = st.value;
+      }
       if (opt.prependHttpIfNeeded && !/^https?:\/\//.test(link)){
         link = "http://" + link;
       }
       // is this text an hyperlink? 
       if(opt.regexp == null || (new RegExp(opt.regexp).test(st.value))){
         var a = $("<a></a>").attr("href",link).addClass("hyperlinkAddIn");
-        a.text(st.value);
+        a.text(label);
         if(opt.openInNewTab){
           a.attr("target","_blank");
         }
@@ -230,7 +243,129 @@
   };
   Dashboards.registerAddIn("Table", "colType", new AddIn(link));
 
+  var circle = {
+    name: "circle",
+    label: "Circle",
+    defaults:{
+      canvasSize: 10,
+      radius: 4,
+      color: 'black',
+      title: function(st) {return "Value: " + st.value;}
+    },
+    
+    implementation: function(tgt, st, opt){
+      var p = $(tgt).empty(),
+        v = st.value,
+        op,
+        options = {},
+        w,
+        paper;
+
+      for (key in opt) if (opt.hasOwnProperty(key)) {
+        op = opt[key];
+        options[key] = typeof op == 'function' ?
+          op.call(this,st):
+          op;
+      }
+      w = options.canvasSize;
+      paper = Raphael(tgt, options.canvasSize, options.canvasSize);
+      var r = paper.circle(w/2,w/2,options.radius);
+      r.attr({
+          fill: options.color,
+          opacity: 1,
+          "stroke-width":0,
+          "title": options.title
+      });
+    }
+    
+  };
+  Dashboards.registerAddIn("Table", "colType", new AddIn(circle));
+
+  var bullet = {
+    name: "cccBulletChart",
+    label: "Bullet Chart",
+    defaults:{
+      height: 40,
+      animate: false,
+      orientation: "horizontal",
+      bulletSize: 16,     // Bullet height
+      bulletSpacing: 150, // Spacing between bullets
+      bulletMargin: 5,   // Left margin
+      // Specific values
+      bulletRanges: [30,80,100],
+      extensionPoints: {
+        "bulletMarker_shape":"triangle",
+        "bulletTitle_textStyle":"green",
+        "bulletMeasure_fillStyle":"black",
+        "bulletRuleLabel_font":"8px sans-serif",
+        "bulletRule_height": 5
+      }
+    },
+
+    init: function(){
+      $.fn.dataTableExt.oSort[this.name+'-asc'] = $.fn.dataTableExt.oSort['string-asc'];
+      $.fn.dataTableExt.oSort[this.name+'-desc'] = $.fn.dataTableExt.oSort['string-desc'];
+    },
+
+    sort: function()  {
+      
+    },
+
+    implementation: function(tgt, st, opt){
+      var chartOptions = $.extend(true,{},opt);
+      var $tgt = $(tgt);
+      var target = $("<span></span>").appendTo($tgt.empty());
+      var values = st.value.split(",");
+      var data = this.getData(values);
+
+      chartOptions.canvas = target.get(0);
+      chartOptions.width = chartOptions.width || $tgt.width();
+      chartOptions.bulletMeasures = [values[0]];
+      chartOptions.bulletMarkers = [values[1]];
  
+      var chart = new pvc.BulletChart(chartOptions);
+      chart.setData(data,{});
+      chart.render();
+    },
+
+    getData: function(values) {
+      var dataSet = {
+          resultset: [values],
+          metadata: []
+        },
+        i;
+      for (i = 0; i < values.length;i++) {
+        dataSet.metadata.push({
+          colIndex: i,
+          colType: "String",
+          colName: ""
+        });
+      }
+      return dataSet;
+    }    
+  };
+  Dashboards.registerAddIn("Table", "colType", new AddIn(bullet));
+
+ var formattedText = {
+    name: "formattedText",
+    label: "Formatted Text",
+    defaults: {
+      textFormat: function(v, st) {return st.colFormat ? sprintf(st.colFormat,v) : v;}
+    },
+
+    init: function(){
+      $.fn.dataTableExt.oSort[this.name+'-asc'] = $.fn.dataTableExt.oSort['string-asc'];
+      $.fn.dataTableExt.oSort[this.name+'-desc'] = $.fn.dataTableExt.oSort['string-desc'];
+    },
+    
+    implementation: function(tgt, st, opt){
+      var text = opt.textFormat.call(this, st.value, st, opt);
+      $(tgt).empty().append(text);
+    }
+    
+  };
+  Dashboards.registerAddIn("Table", "colType", new AddIn(formattedText));
+
 })();
 
 
