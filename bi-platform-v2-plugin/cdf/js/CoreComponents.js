dab564155ef40c6a41bce79a144491316c57640d
Pedro Pinheiro <pdpinheiro@gmail.com>
2012-01-10 10:52:44 +0000

    Fix a bad commit

--- bi-platform-v2-plugin/cdf/js/CoreComponents.js
+++ bi-platform-v2-plugin/cdf/js/CoreComponents.js
@@ -2251,13 +2251,13 @@ var QueryComponent = BaseComponent.extend({
   visible: false,
   update : function() {
     QueryComponent.makeQuery(this);
-  }
-},
+  },
   warnOnce: function() {
   Dashboards.log("Warning: QueryComponent behaviour is due to change. See " +
     "http://http://www.webdetails.org/redmine/projects/cdf/wiki/QueryComponent" + 
     " for more information");
     delete(this.warnOnce);
+  }
 },
 {
   makeQuery: function(object){
