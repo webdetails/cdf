aa46cba7c131f39583e3179ecce82f9b78d4277c
Pedro Pinheiro <pdpinheiro@gmail.com>
2012-03-06 11:28:13 +0000

    Shim Object.keys in Dashboards.js

--- bi-platform-v2-plugin/cdf/js/Dashboards.js
+++ bi-platform-v2-plugin/cdf/js/Dashboards.js
@@ -15,6 +15,40 @@ if ( !String.prototype.endsWith ) {
   };
 } 
 
+if (!Object.keys) {
+  Object.keys = (function () {
+    var hasOwnProperty = Object.prototype.hasOwnProperty,
+        hasDontEnumBug = !({toString: null}).propertyIsEnumerable('toString'),
+        dontEnums = [
+          'toString',
+          'toLocaleString',
+          'valueOf',
+          'hasOwnProperty',
+          'isPrototypeOf',
+          'propertyIsEnumerable',
+          'constructor'
+        ],
+        dontEnumsLength = dontEnums.length
+
+    return function (obj) {
+      if (typeof obj !== 'object' && typeof obj !== 'function' || obj === null) throw new TypeError('Object.keys called on non-object')
+
+      var result = []
+
+      for (var prop in obj) {
+        if (hasOwnProperty.call(obj, prop)) result.push(prop)
+      }
+
+      if (hasDontEnumBug) {
+        for (var i=0; i < dontEnumsLength; i++) {
+          if (hasOwnProperty.call(obj, dontEnums[i])) result.push(dontEnums[i])
+        }
+      }
+      return result
+    }
+  })()
+};
+
 // Production steps of ECMA-262, Edition 5, 15.4.4.19  
 // Reference: http://es5.github.com/#x15.4.4.19  
 if (!Array.prototype.map) {  
