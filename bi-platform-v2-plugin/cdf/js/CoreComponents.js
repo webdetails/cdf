2b75ea2ef6b90e9351c00a8b75e6559d023cd2c2
diogofscmariano <diogo.mariano@webdetails.pt>
2012-05-23 14:56:27 +0100

    Fixed bugs on TextInputComponent html object. Added TextareaInputComponent

--- bi-platform-v2-plugin/cdf/js/CoreComponents.js
+++ bi-platform-v2-plugin/cdf/js/CoreComponents.js
@@ -1003,11 +1003,13 @@ var TextComponent = BaseComponent.extend({
 var TextInputComponent = BaseComponent.extend({
   update: function(){
     selectHTML = "<input";
-    selectHTML += " type=test id='" + this.name + "' name='" + this.name +
-    (this.charWidth ? ("' + size='" + this.charWidth) : "") +
-    (this.maxChars ? ("' + maxlength='" + this.maxChars) : "") +
+    selectHTML += " type=text id='" + this.name + "' name='" + this.name +
+    "'  value='" +
+    Dashboards.getParameterValue(this.parameter) +
+    (this.size ? ("' size='" + this.size) : "") +
+    (this.maxLength ? ("' maxlength='" + this.maxLength) : "") +
     "'>";
-    $("#" + this.htmlObject).append($(selectHTML).val(Dashboards.getParameterValue(this.parameter)));
+    $("#" + this.htmlObject).html(selectHTML);
     var myself = this;
     $("#" + this.name).change(function(){
       Dashboards.processChange(myself.name);
@@ -1021,6 +1023,31 @@ var TextInputComponent = BaseComponent.extend({
     return $("#"+this.name).val();
   }
 });
+
+
+var TextareaInputComponent = BaseComponent.extend({
+  update: function(){
+    selectHTML = "<textarea";
+    selectHTML += " id='" + this.name + "' name='" + this.name +
+    (this.numColumns ? ("' rows='" + this.rows) : "") +
+    (this.numColumns ? ("' cols='" + this.numColumns) : "") +
+    "'>" + Dashboards.getParameterValue(this.parameter) + 
+    '</textarea>';
+    $("#" + this.htmlObject).html(selectHTML);
+    var myself = this;
+    $("#" + this.name).change(function(){
+      Dashboards.processChange(myself.name);
+    }).keyup(function(event){
+      if (event.keyCode == 13) {
+        Dashboards.processChange(myself.name);
+      }
+    });
+  },
+  getValue : function() {
+    return $("#"+this.name).val();
+  }
+});
+
 
 
 // Start by setting a sane i18n default to datepicker
