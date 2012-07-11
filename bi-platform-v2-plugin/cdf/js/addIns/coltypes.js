6139c1719ae06dda1cdcac1e29dbf3c2d76f4a51
Pedro Pinheiro <pdpinheiro@gmail.com>
2012-07-11 20:03:25 +0100

    add column headers support to group headers addIn

--- bi-platform-v2-plugin/cdf/js/addIns/coltypes.js
+++ bi-platform-v2-plugin/cdf/js/addIns/coltypes.js
@@ -423,7 +423,9 @@
     name: "groupHeaders",
     label: "Group Headers",
     defaults: {
-      hide:true
+      hide:true,
+      columnHeadersInGroups: false,
+      replaceFirstHeader: true
     },
 
     init: function(){
@@ -435,8 +437,15 @@
       var dt = $(tgt).parents('table').eq(0).dataTable(),
           visColIdx = $(tgt).index();
 
+      /* Decide whether to hide the original column we're drawing the group headers from */
       if (opt.hide){
          dt.find('.groupHeaders:nth-child(' + (visColIdx + 1) + ')').addClass('hiddenCol');
+      }
+
+      
+      if(opt.columnHeadersInGroups) {
+        var header = dt.find("thead").eq(0);
+        header.find("tr").clone
       }
 
       var $row = $(dt.fnGetNodes( st.rowIdx )),
@@ -444,15 +453,46 @@
           count = $row.children().length,
           $group;
 
+      /* We create and insert a group header under any of the following circumstances:
+       *   - On the very first row
+       *   - Immediately after a higher-level group header
+       *     when using group headers for more than one column
+       *   - when the value for the current cell is
+       *     different from the one immediately before it
+       */
       if ( visRowIdx === 0 || $row.prev().hasClass('groupHeader') || ( st.value != dt.fnGetData( $row.prev().get(0) )[st.colIdx]  )) {
-          $group = $('<td/>').append( st.value );
-          $group.attr({colSpan:count});
-          $group = $('<tr/>').addClass('groupHeader group' + visColIdx).append($group);
+          $group = this.buildHeader(tgt,st, opt);
           $group.insertBefore($row);
       }
  
-    }
+    },
 
+    buildHeader: function(tgt, st, opt) {
+      var $header,
+          $dt = $(tgt).parents('table').eq(0).dataTable(),
+          $theader;
+
+      if(opt.columnHeadersInGroups) {
+        $theader = $dt.find("thead").eq(0);
+        $theader.hide();
+        $header = $("<tr>");
+        $theader.find("tr th").each(function(i,e){
+          var $e = $(e),
+              newCell = $("<td>").text($e.text()).width(e.style.width);
+          newCell.addClass($(e).hasClass("hiddenCol")? "hiddenCol" : "");
+          $header.append(newCell);
+        });
+        $header.find("td").eq($(tgt).index() + 1).text(st.value).addClass("groupName");
+      } else {
+        $header = $("<tr/>");
+        $("<td/>").addClass("groupName").text(st.value).attr("colspan",  $(tgt).siblings().length + 1).appendTo($header);
+      }
+      $header.addClass("groupHeader group" + $(tgt).index());
+      var $preSpace = $("<td>").attr("colspan",$(tgt).siblings().length + 1).wrap("<tr>").parent().addClass("groupHeader preSpace");
+      var $postSpace = $("<td>").attr("colspan",$(tgt).siblings().length + 1).wrap("<tr>").parent().addClass("groupHeader postSpace");
+      var $response = $preSpace.add($header).add($postSpace);
+      return $response;
+    }
   };
   Dashboards.registerAddIn("Table", "colType", new AddIn(groupHeaders));
 
