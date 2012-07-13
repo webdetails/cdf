46d068eea28bd72af9b3e4552099656b4f1d0390
Pedro Pinheiro <pdpinheiro@gmail.com>
2012-07-13 11:29:27 +0100

    Add clippedText AddIn
    This AddIn will truncate text so table cells won't overflow horribly, while optionally providing a tooltip so you can see the complete cell text.

--- bi-platform-v2-plugin/cdf/js/addIns/coltypes.js
+++ bi-platform-v2-plugin/cdf/js/addIns/coltypes.js
@@ -496,6 +496,35 @@
   };
   Dashboards.registerAddIn("Table", "colType", new AddIn(groupHeaders));
 
+  var clippedText = {
+    name: "clippedText",
+    label: "Clipped Text",
+    defaults: {
+      showTooltip: true,
+      useTipsy: false,
+      style: {}
+    },
+
+    init: function(){
+      $.fn.dataTableExt.oSort[this.name+'-asc'] = $.fn.dataTableExt.oSort['string-asc'];
+      $.fn.dataTableExt.oSort[this.name+'-desc'] = $.fn.dataTableExt.oSort['string-desc'];
+    },
+    
+    implementation: function(tgt, st, opt){
+      var $tgt = $(tgt),
+          $container = $("<div>");
+      $tgt.empty().append($container);
+      $container.text(st.value).addClass("clippedText").attr("title",opt.showTooltip ? st.value : "");
+      $container.css(opt.style);
+      if(opt.useTipsy) {
+        $container.tipsy({
+            gravity: 's', 
+            html:false
+        });
+      }
+    }
+  };
+  Dashboards.registerAddIn("Table", "colType", new AddIn(clippedText));
 
 })();
 
