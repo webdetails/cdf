667195ebd7e667039d612a3fc3bb0e7a0e3d3eea
Pedro Pinheiro <pdpinheiro@gmail.com>
2012-10-01 17:20:12 +0100

    Update Views to use base64 to kick params back and forth.
    Stupid orientdb doesn't cope with json values all that well.

--- bi-platform-v2-plugin/cdf/js/Dashboards.js
+++ bi-platform-v2-plugin/cdf/js/Dashboards.js
@@ -883,7 +883,7 @@ Dashboards.fireChange = function(parameter, value) {
 Dashboards.restoreView = function() {
   var p, params;
   if(!Dashboards.view) return;
-  params = Dashboards.view.params;
+  params = JSON.parse(Base64.decode(Dashboards.view.params));
   for(p in params) if (params.hasOwnProperty(p)) {
     Dashboards.setParameter(p,params[p]);
   }
