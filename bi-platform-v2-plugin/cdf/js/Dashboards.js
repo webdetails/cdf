1bb0fb026d3002d0d7261216d4d23888a1119c0b
Pedro Vale <pedro.vale@webdetails.pt>
2012-11-05 12:07:24 +0000

    Changed exportData so it does not break with url length

--- bi-platform-v2-plugin/cdf/js/Dashboards.js
+++ bi-platform-v2-plugin/cdf/js/Dashboards.js
@@ -4092,15 +4092,19 @@ Query = function() {
       queryDefinition.settingcolumnHeaders = options.columnHeaders;
 
     }
+  }
+    
+  var theDoQuery = CDA_PATH + $.param(queryDefinition);
+  $.post(theDoQuery, function(uuid) {
 
     _exportIframe = _exportIframe || $('<iframe style="display:none">');
 
     _exportIframe.detach();
-
-    _exportIframe[0].src = CDA_PATH + $.param(queryDefinition);
-
+    _exportIframe[0].src = webAppPath + '/content/cda/unwrapQuery?' + $.param( {"path": queryDefinition.path, "uuid": uuid});
     _exportIframe.appendTo($('body'));
-
+  });    
+    
+    
   }
 
 
