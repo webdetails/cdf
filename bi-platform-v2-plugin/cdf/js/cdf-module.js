b2619da3cd4b31d97d7a60fe1cd404b66d971f92
Pedro Vale <pedro.vale@webdetails.pt>
2012-11-09 15:33:00 +0000

    4.8 ee compatibility

---
+++ bi-platform-v2-plugin/cdf/js/cdf-module.js
@@ -0,0 +1,20 @@
+define(['cdf/Base',
+  'cdf/Dashboards',
+  'cdf/jquery',
+  'cdf/jquery.ui',
+  'cdf/jquery-impromptu.3.1',
+  'cdf/jquery.bgiframe',
+  'cdf/jquery.blockUI',
+  'cdf/jquery.corner',
+  'cdf/jquery.eventstack',
+  'cdf/jquery.i18n.properties',
+  'cdf/jquery.jdMenu',
+  'cdf/jquery.positionBy',
+  'cdf/jquery.sparkline',
+  'cdf/jquery.tooltip',
+  'cdf/json',
+  'cdf/simile/ajax/simile-ajax-api',
+  'cdf/simile/ajax/scripts/json',
+  'cdf/CoreComponents'], function(){
+	
+});
