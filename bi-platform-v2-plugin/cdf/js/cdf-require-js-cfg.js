b324c70d9f63d1ea85e07ce94e942cc7d3c7c282
Nick Baker <nbaker@pentaho.com>
2012-12-06 09:42:47 -0500

    Fixing shim configuration for Tipsy. Ported build.xml code from 4.8 which handles wiping out the cdf-module.js which should be empty at deployment.

--- bi-platform-v2-plugin/cdf/js/cdf-require-js-cfg.js
+++ bi-platform-v2-plugin/cdf/js/cdf-require-js-cfg.js
@@ -1,66 +1,66 @@
 requireCfg['paths']['cdf'] = CONTEXT_PATH+'content/pentaho-cdf/js';
 requireCfg['shim']['cdf/cdf-module'] = ['cdf/Dashboards',
-	'cdf/jquery.ui',
-	'cdf/jquery-impromptu.3.1',
-	'cdf/jquery-ui-datepicker-i18n',
-	'cdf/jquery.bgiframe',
-	'cdf/jquery.blockUI',
-	'cdf/jquery.corner',
-	'cdf/jquery.eventstack',
-	'cdf/jquery.i18n.properties',
-	'cdf/jquery.jdMenu',
-	'cdf/jquery.positionBy',
-	'cdf/jquery.sparkline',
-	'cdf/jquery.tooltip',
-	'cdf/simile/ajax/simile-ajax-api',
-	'cdf/simile/ajax/scripts/json',
-	'cdf/json',
-    'cdf/lib/shims',
-    'cdf/lib/CCC/pvc-d1.0',
-    'cdf/lib/CCC/protovis', 
-    'cdf/lib/CCC/tipsy', 
-    'cdf/lib/CCC/jquery.tipsy', 
-    'cdf/lib/CCC/def',    
-	'cdf/backbone',
-	'cdf/underscore',
-	'cdf/mustache',
-	'cdf/components/ccc',
-	'cdf/components/core',
-    'cdf/components/input'	,
-    'cdf/components/jfreechart',    
-    'cdf/components/maps',
-    'cdf/components/navigation',
-    'cdf/components/pentaho',
-    'cdf/components/simpleautocomplete',
-	'cdf/components/table'];
+  'cdf/jquery.ui',
+  'cdf/jquery-impromptu.3.1',
+  'cdf/jquery-ui-datepicker-i18n',
+  'cdf/jquery.bgiframe',
+  'cdf/jquery.blockUI',
+  'cdf/jquery.corner',
+  'cdf/jquery.eventstack',
+  'cdf/jquery.i18n.properties',
+  'cdf/jquery.jdMenu',
+  'cdf/jquery.positionBy',
+  'cdf/jquery.sparkline',
+  'cdf/jquery.tooltip',
+  'cdf/simile/ajax/simile-ajax-api',
+  'cdf/simile/ajax/scripts/json',
+  'cdf/json',
+  'cdf/lib/shims',
+  'cdf/lib/CCC/pvc-d1.0',
+  'cdf/lib/CCC/protovis',
+  'cdf/lib/CCC/tipsy',
+  'cdf/lib/CCC/jquery.tipsy',
+  'cdf/lib/CCC/def',
+  'cdf/backbone',
+  'cdf/underscore',
+  'cdf/mustache',
+  'cdf/components/ccc',
+  'cdf/components/core',
+  'cdf/components/input'	,
+  'cdf/components/jfreechart',
+  'cdf/components/maps',
+  'cdf/components/navigation',
+  'cdf/components/pentaho',
+  'cdf/components/simpleautocomplete',
+  'cdf/components/table'];
 
 
 requireCfg['shim']['cdf/CoreComponents'] = [
-	'cdf/components/core',
-	'cdf/components/ccc',
-    'cdf/components/input'	,
-    'cdf/components/jfreechart',    
-    'cdf/components/maps',
-    'cdf/components/navigation',
-    'cdf/components/pentaho',
-    'cdf/components/simpleautocomplete',
-	'cdf/components/table'
+  'cdf/components/core',
+  'cdf/components/ccc',
+  'cdf/components/input'	,
+  'cdf/components/jfreechart',
+  'cdf/components/maps',
+  'cdf/components/navigation',
+  'cdf/components/pentaho',
+  'cdf/components/simpleautocomplete',
+  'cdf/components/table'
 ];
 
 
 
 requireCfg['shim']['cdf/Dashboards'] = [
-    'cdf/Base',
-    'cdf/underscore',
-    'cdf/backbone',
-    'cdf/mustache', 
-    'cdf/lib/shims'    
+  'cdf/Base',
+  'cdf/underscore',
+  'cdf/backbone',
+  'cdf/mustache',
+  'cdf/lib/shims'
 ];
 
 
 requireCfg['shim']['cdf/backbone'] = ['cdf/underscore'];
 requireCfg['shim']['cdf/lib/CCC/pvc-d1.0'] = ['cdf/lib/CCC/protovis', 'cdf/lib/CCC/tipsy', 'cdf/lib/CCC/jquery.tipsy', 'cdf/lib/CCC/def'];
-
+requireCfg['shim']['cdf/lib/CCC/tipsy'] = ['cdf/lib/CCC/protovis'];
 requireCfg['shim']['cdf/components/core'] = ['cdf/Dashboards'];
 requireCfg['shim']['cdf/components/ccc'] = ['cdf/components/core', 'cdf/lib/CCC/pvc-d1.0'];
 requireCfg['shim']['cdf/components/input'] = ['cdf/components/core'];
