0ba6dd8a3e7b161372f790caa107aa1b30082ef7
Pedro Pinheiro <pdpinheiro@gmail.com>
2012-12-13 16:04:25 +0000

    Add support for arbitrary user data in Views

--- bi-platform-v2-plugin/src/org/pentaho/cdf/views/View.java
+++ bi-platform-v2-plugin/src/org/pentaho/cdf/views/View.java
@@ -1,7 +1,6 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
-
 package org.pentaho.cdf.views;
 
 import java.util.ArrayList;
@@ -22,8 +21,9 @@ import pt.webdetails.cpf.persistence.Persistable;
 public class View implements Persistable {
 
     private Map<String, Object> parameters;
+    private Map<String, Object> userData;
     private List<String> unboundParams;
-    private String name, id, user, description, key;
+    private String name, viewId, user, description, key;
     private String solution, path, file;
     private Date timestamp;
 
@@ -59,6 +59,14 @@ public class View implements Persistable {
         return parameters.get(name);
     }
 
+    public void setUserData(String name, Object value) {
+        userData.put(name, value);
+    }
+
+    public Object getUserData(String name) {
+        return userData.get(name);
+    }
+
     public String getName() {
         return name;
     }
@@ -68,11 +76,11 @@ public class View implements Persistable {
     }
 
     public String getId() {
-        return id;
+        return viewId;
     }
 
     public void setId(String id) {
-        this.id = id;
+        this.viewId = id;
     }
 
     public String getUser() {
@@ -109,16 +117,20 @@ public class View implements Persistable {
 
     public JSONObject toJSON() {
         try {
-            String p = new String(Base64.encodeBase64(new JSONObject(parameters).toString(2).getBytes()));
             JSONObject json = new JSONObject();
             json.put("description", description);
             json.put("name", name);
-            json.put("id", id);
+            json.put("id", viewId);
             json.put("key", key);
             json.put("timestamp", timestamp == null ? 0 : timestamp.getTime());
             json.put("user", user);
             json.put("unbound", new JSONArray(unboundParams));
+            String p = new String(Base64.encodeBase64(new JSONObject(parameters).toString(2).getBytes()));
             json.put("params", p);
+            if (userData != null) {
+                String u = new String(Base64.encodeBase64(new JSONObject(userData).toString(2).getBytes()));
+                json.put("userData", u);
+            }
             json.put("solution", solution);
             json.put("path", path);
             json.put("file", file);
@@ -132,19 +144,21 @@ public class View implements Persistable {
         try {
             String _description, _name, _id, _key, _user, _solution, _path, _file;
             Date _timestamp;
+            JSONObject jsonUserData, jsonParams;
+            Map<String, Object> _params = new HashMap<String, Object>(),
+                    _userData = new HashMap<String, Object>();
             _description = json.optString("description");
             _name = json.getString("name");
             _id = json.getString("id");
-            _timestamp = new Date(json.optLong("timestamp",0));
+            _timestamp = new Date(json.optLong("timestamp", 0));
             _user = json.getString("user");
             _key = json.isNull("key") ? null : json.optString("key");
             _solution = json.getString("solution");
             _path = json.getString("path");
             _file = json.getString("file");
-            String p =json.getString("params");
-            JSONObject jsonParams = new JSONObject(new String(Base64.decodeBase64(p.getBytes())));
+            String p = json.getString("params");
+            jsonParams = new JSONObject(new String(Base64.decodeBase64(p.getBytes())));
             JSONArray jsonUnbound = json.getJSONArray("unbound");
-            Map<String, Object> _params = new HashMap<String, Object>();
             String[] keys = JSONObject.getNames(jsonParams);
             if (keys != null) {
                 for (String k : keys) {
@@ -155,13 +169,27 @@ public class View implements Persistable {
             for (int i = 0; i < jsonUnbound.length(); i++) {
                 _unbound.add(jsonUnbound.getString(i));
             }
+
+            if (json.has("userData")) {
+                String u = json.getString("userData");
+                jsonUserData = new JSONObject(new String(Base64.decodeBase64(u.getBytes())));
+                String[] userDataKeys = JSONObject.getNames(jsonUserData);
+                if (userDataKeys != null) {
+                    for (String k : userDataKeys) {
+                        _userData.put(k, jsonUserData.get(k));
+                    }
+                }
+            } else {
+                _userData = null;
+            }
             name = _name;
             user = _user;
             description = _description;
-            id = _id;
+            viewId = _id;
             key = _key;
             timestamp = _timestamp;
             parameters = _params;
+            userData = _userData;
             unboundParams = _unbound;
             solution = _solution;
             path = _path;
