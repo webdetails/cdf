5989c40f9967e8e60a103744fde6c192fbdab8ad
Pedro Martins <Pedro.Martins@webdetails.pt>
2013-01-02 11:37:24 +0000

    Changed the way "earliestDate" and "latestDate" are retrieving their value. Either set by a property, or with a default value (no Dashboards.getParamaterValue)

--- bi-platform-v2-plugin/cdf/js/components/input.js
+++ bi-platform-v2-plugin/cdf/js/components/input.js
@@ -281,8 +281,8 @@ var DateRangeInputComponent = BaseComponent.extend({
     }
     var offset = dr.offset();
     var myself = this;
-    var earliestDate = this.earliestDate != undefined  ?  Dashboards.getParameterValue(this.earliestDate) : Date.parse('-1years');
-    var latestDate = this.latestDate != undefined  ?  Dashboards.getParameterValue(this.latestDate) : Date.parse('+1years');
+    var earliestDate = this.earliestDate != undefined  ?  this.earliestDate : Date.parse('-1years');
+    var latestDate = this.latestDate != undefined  ?  this.latestDate : Date.parse('+1years');
     var leftOffset = this.leftOffset != undefined ?  this.leftOffset : 0;
     var topOffset = this.topOffset != undefined ?  this.topOffset : 15;
     
