99daa76f89dba8d8c01147a4aa074c257a48d9cf
tgf <tgf@segafredo.(none)>
2013-01-21 16:58:28 +0000

    packager: close streams, use more ioutils

--- bi-platform-v2-plugin/src/pt/webdetails/packager/Concatenate.java
+++ bi-platform-v2-plugin/src/pt/webdetails/packager/Concatenate.java
@@ -1,7 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 package pt.webdetails.packager;
 
 import java.io.BufferedReader;
@@ -14,14 +14,19 @@ import java.io.InputStream;
 import java.io.SequenceInputStream;
 import java.util.Enumeration;
 import java.util.NoSuchElementException;
+
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
-import org.pentaho.platform.engine.core.system.PentahoSystem;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 
 /**
  *
  * @author pdpi
  */
 class Concatenate {
+  private static final Log logger = LogFactory.getLog(Concatenate.class);
 
   public static InputStream concat(File[] files) {
     ListOfFiles mylist = new ListOfFiles(files);
@@ -33,11 +38,13 @@ class Concatenate {
     if (rootpath == null || StringUtils.isEmpty(rootpath)) {
       return concat(files);
     }
+    BufferedReader fr = null;
     try {
       StringBuffer buffer = new StringBuffer();
       for (File file : files) {
+        try {
         StringBuffer tmp = new StringBuffer();
-        BufferedReader fr = new BufferedReader(new FileReader(file));
+        fr = new BufferedReader(new FileReader(file));
         while (fr.ready()) {
           tmp.append(fr.readLine());
         }
@@ -47,12 +54,25 @@ class Concatenate {
                 // We need to replace all the URL formats
                 .replaceAll("(url\\(['\"]?)", "$1" + fileLocation) // Standard URLs
                 .replaceAll("(progid:DXImageTransform.Microsoft.AlphaImageLoader\\(src=')", "$1" + fileLocation + "../")); // these are IE-Only
+        }
+        catch (FileNotFoundException e) {
+          logger.error("concat: File " + file.getAbsolutePath()
+              + " doesn't exist! Skipping...");
+        }
+        catch (Exception e) {
+          logger.error("concat: Error while attempting to concatenate file "
+              + file.getAbsolutePath() + ". Attempting to continue", e);
+        }
+        finally {
+          IOUtils.closeQuietly(fr);
+        }
 
       }
       return new ByteArrayInputStream(buffer.toString().getBytes("UTF8"));
     } catch (Exception e) {
       return null;
     }
+
   }
 }
 
