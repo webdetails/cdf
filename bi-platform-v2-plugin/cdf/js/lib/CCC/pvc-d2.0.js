929ce8e6c4f8385307173849e8e9a904e4798e18
Duarte Cunha Leao <duarte.leao@gmail.com>
2013-04-08 16:42:37 +0100

    [FIX](CCC scene var valueOf) removed the valueOf method, that would gain to the toString method, when returning a number.

--- bi-platform-v2-plugin/cdf/js/lib/CCC/pvc-d2.0.js
+++ bi-platform-v2-plugin/cdf/js/lib/CCC/pvc-d2.0.js
@@ -11830,8 +11830,9 @@ def.set(
         return label == null ? "" :
                (typeof label !== 'string') ? ('' + label) :
                label;
-    },
-    'valueOf', function() { return this.value; });
+    }
+    //'valueOf', function() { return this.value; }
+    );
 
 pvc_ValueLabelVar.fromComplex = function(complex) {
     return complex ?
