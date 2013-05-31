2b23393c3328a49725ecaa7701fd630e4576c4dc
Pedro Teixeira <pedrofvteixeira@gmail.com>
2013-05-31 11:17:02 +0100

    [CDF - Bug #271] TableComponent with server side pag. hangs with "processing" when query fails

--- bi-platform-v2-plugin/cdf/js/Dashboards.js
+++ bi-platform-v2-plugin/cdf/js/Dashboards.js
@@ -3798,10 +3798,21 @@ Query = function() {
 
     }
     var successHandler = function(json) {
-      if(_mode == 'Legacy'){
-
-        json = eval("(" + json + ")");
-
+	if(_mode == 'Legacy'){
+        try{
+          json = eval("(" + json + ")");
+        }catch(e){
+          if(this.async){
+            // async + legacy errors while parsing json response aren't caught
+            var msg = Dashboards.getErrorObj('COMPONENT_ERROR').msg + ":" + e.message;
+            Dashboards.error(msg);
+            json = {"metadata":[msg],"values":[]};
+          }else{
+            //exceptions while parsing json response are 
+            //already being caught+handled in updateLifecyle()  
+            throw e;
+          }  
+        }
       }
 
       _lastResultSet = json;
