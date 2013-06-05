a678527f0fc93b23d7e2611effe120fe05571da0
Goncalo Neto <goncalo.neto@webdetails.pt>
2013-06-05 21:22:42 +0100

    Add Container class and OptionsManager class to Dashboards.

--- bi-platform-v2-plugin/cdf/js/Dashboards.js
+++ bi-platform-v2-plugin/cdf/js/Dashboards.js
@@ -2372,66 +2372,250 @@ sprintfWrapper = {
 
 sprintf = sprintfWrapper.init;
 
-//Normalization - Ensure component does not finish with component and capitalize first letter
-Dashboards.normalizeAddInKey = function(key) {
-    if (key.indexOf('Component', key.length - 'Component'.length) !== -1)
-      key = key.substring(0, key.length - 'Component'.length);
-  return key.charAt(0).toUpperCase() + key.substring(1);
-}
 
-Dashboards.registerAddIn = function(component,slot,addIn){
-  if (!this.addIns) {
-    this.addIns = {};
-  }
+// CONTAINER begin
+;(function (D){
 
+    function Container() {
 
-  var key = this.normalizeAddInKey(component);
+        // PUBLIC
 
+        // register(type, what [, scope])
+        // register(type, name, what [, scope])
+        this.register = function(type, name, what, scope) {
+            if(!type) { throw new Error("Argument 'type' is required."); }
+            if(typeof type !== 'string') { throw new Error("Argument 'type' must be a string."); }
 
-  if (!this.addIns[key]) {
-    this.addIns[key] = {};
-  }
-  if (!this.addIns[key][slot]) {
-    this.addIns[key][slot] = {};
-  }
-  this.addIns[key][slot][addIn.getName()] = addIn;
-};
+            if(name != null) {
+                if(typeof name !== 'string') {
+                    scope = what;
+                    what  = name;
+                    name  = null;
+                } else if(!name) {
+                    name = null;
+                }
+            }
 
-Dashboards.hasAddIn = function(component,slot,addIn){
-  var key = this.normalizeAddInKey(component);
-  return Boolean(this.addIns && this.addIns[key] &&
-    this.addIns[key][slot] && this.addIns[key][slot][addIn]);
-};
+            if(!what) { throw new Error("Argument 'what' is required."); }
 
-Dashboards.getAddIn = function(component,slot,addIn){
-  var key = this.normalizeAddInKey(component);
-  try {
-    return this.addIns[key][slot][addIn];
-  } catch (e) {
-    return null;
-  }
-};
+            var holder;
+            switch(typeof what) {
+                case 'function': holder = new FactoryHolder (this, what, scope); break;
+                case 'object':   holder = new InstanceHolder(this, what, scope); break;
+                default: throw new Error("Argument 'what' is of an invalid type.");
+            }
 
-Dashboards.setAddInDefaults = function(component, slot, addInName, defaults) {
-var key = this.normalizeAddInKey(component);
-  var addIn = this.getAddIn(key,slot,addInName);
-  if(addIn) {
-    addIn.setDefaults(defaults);
-  }
-};
-Dashboards.listAddIns = function(component,slot) {
-var key = this.normalizeAddInKey(component);
-  var addInList = [];
-  try {
-    slot = this.addIns[key][slot];
-    for (var addIn in slot) if (slot.hasOwnProperty(addIn)) {
-      addInList.push([addIn, slot[addIn].getLabel()]);
+            if(!name) { name = ''; }
+
+            var holdersByName = _typesTable[type] || (_typesTable[type] = {});
+            var currHolder = holdersByName[name];
+            if(currHolder) {
+                // throw? log?
+                currHolder.dispose();
+            }
+            holdersByName[name] = holder;
+        };
+
+        this.has    = function(type, name) { return !!getHolder(type, name, true); };
+        this.canNew = function(type, name) { return getHolder(type, name, false) instanceof FactoryHolder; };
+
+        this.get       = function(type, name)         { return get(type, name, null,   false, false); };
+        this.tryGet    = function(type, name)         { return get(type, name, null,   false, true ); };
+
+        this.getNew    = function(type, name, config) { return get(type, name, config, true,  false); };
+        this.tryGetNew = function(type, name, config) { return get(type, name, config, true,  true ); };
+
+        this.getAll    = function(type) { return getAll(type, false); };
+        this.tryGetAll = function(type) { return getAll(type, true ); };
+
+        this.dispose = function() {
+            if(_typesTable) {
+                for(var type in _typesTable) {
+                    var holdersByName = _typesTable[type];
+                    for(var name in holdersByName) {
+                        holdersByName[name].dispose();
+                    }
+                }
+
+                _typesTable = null;
+            }
+        };
+
+        // PRIVATE
+
+        var _typesTable = {}; // type -> []
+
+        function getType(type, isTry) {
+            if(!type) { throw new Error("Argument 'type' is required."); }
+            if(typeof type !== 'string') { throw new Error("Argument 'type' must be a string."); }
+
+            var holdersByName = _typesTable[type];
+            if(!isTry && (!holdersByName || isOwnEmpty(holdersByName))) {
+                throw new Error("There are no registrations for type '" + type + "'.");
+            }
+            return holdersByName;
+        }
+
+        function getHolder(type, name, isTry) {
+            var holder;
+            var holdersByName = getType(type, isTry);
+            if(holdersByName) {
+                holder = holdersByName[name || ''];
+                if(!holder && !isTry) {
+                    throw new Error(
+                        "There is no registration for type '" + type + "'" +
+                        (name ? (" and name '" + name + "'") : "") + ".");
+                }
+            }
+
+            return holder;
+        }
+
+        function get(type, name, config, isNew, isTry) {
+            if(typeof name !== 'string') {
+                config = name;
+                name = '';
+            }
+
+            var holder = getHolder(type, name, isTry);
+
+            // Can't store as singletons instances with special config params
+            if(config) { isNew = true;  } else
+            if(!isNew) { config = null; }
+
+            return holder ? holder.build(config, isNew) : null;
+        }
+
+        function getAll(type, isTry) {
+            var holdersByName = getType(type, isTry);
+
+            // Includes the default (unnamed) instance
+            var instances = [];
+            for(var name in holdersByName) {
+                instances.push(holdersByName[name].build(null, false));
+            }
+            return instances;
+        }
     }
-    return addInList;
-  } catch (e) {
-    return [];
+
+    // Shared/Static stuff
+
+    // Allows creating multiple instances
+    function FactoryHolder(container, factory, scope) {
+        var instance;
+
+        if(!scope) { scope = 'instance'; }
+
+        this.build = function(config, buildNew) {
+            if(instance && !buildNew) { return instance; }
+
+            var inst = factory(container, config);
+
+            if(!buildNew && scope === 'singleton') { instance = inst; }
+
+            return inst;
+        };
+
+        this.dispose = function() {
+            if(instance) {
+                doDispose(instance);
+                instance = null;
+            }
+        };
+    }
+
+    function InstanceHolder(container, instance, scope) {
+        if(!scope) { scope = 'external'; }
+
+        this.build = function(/*config, buildNew*/) { return instance; };
+
+        // external scope is managed outside the container
+        this.dispose = function() {
+            if(instance) {
+                scope === 'singleton' && doDispose(instance);
+                instance = null;
+            }
+        };
+    }
+
+    // Fwk stuff
+
+    function doDispose(instance) {
+        if(typeof instance.dispose === 'function') { instance.dispose(); }
+    }
+
+    var hasOwn = Object.prototype.hasOwnProperty;
+
+    function isOwnEmpty(o) {
+        // tolerates o == null
+        for(var n in o) { if(hasOwn.call(o, n)) { return false; } }
+        return true;
+    }
+
+    // Export
+    D.Container = Container;
+})(Dashboards);
+// CONTAINER end 
+
+
+// ADDINS begin
+;(function (D){
+  D.addIns = new D.Container ();
+
+  //Normalization - Ensure component does not finish with component and capitalize first letter
+  D.normalizeAddInKey = function(key, subKey) {
+      if (key.indexOf('Component', key.length - 'Component'.length) !== -1) 
+        key = key.substring(0, key.length - 'Component'.length);  
+      key = key.charAt(0).toUpperCase() + key.substring(1);
+
+      if(subKey) { key += "." + subKey; }
+
+    return key;
   }
-};
+
+  D.registerAddIn = function(type,subType,addIn){
+    var type = this.normalizeAddInKey(type, subType),
+        name = addIn.getName ? addIn.getName() : null;
+    this.addIns.register(type, name, addIn);
+  };
+
+  D.hasAddIn = function(type,subType,addInName){
+    var type = this.normalizeAddInKey(type, subType);
+    return Boolean(this.addIns && this.addIns.has(type,addInName));
+  };
+
+  D.getAddIn = function(type,subType,addInName){
+    var type = this.normalizeAddInKey(type, subType);
+    try {
+      var addIn = this.addIns.get(type,addInName);
+      return addIn;
+    } catch (e) {
+      return null;
+    }
+  };
+
+  D.setAddInDefaults = function(type, subType, addInName, defaults) {
+    var addIn = this.getAddIn(type, subType,addInName);
+    if(addIn) {
+      addIn.setDefaults(defaults);
+    }
+  };
+  D.listAddIns = function(type, subType) {
+  var type = this.normalizeAddInKey(type, subType);
+    var addInList = [];
+    try {
+      return this.addIns.getAll(type);
+    } catch (e) {
+      return [];
+    }
+  };
+})(Dashboards);
+// ADDINS end
+
+
+
+
+
 
 Dashboards.safeClone = function(){
   var options, name, src, copy, copyIsArray, clone,
@@ -2493,6 +2677,141 @@ Dashboards.safeClone = function(){
 
 
 
+// OPTIONS MANAGER begin
+;(function (D){
+
+  // This class is intended to be used as a generic Options Manager, by providing a way to
+  // keep record of the values of an options set, but also custom readers, writers and validators 
+  // for each of the options.
+  function OptionsManager (config ){ /* { defaults: {}, interfaces: {}, libraries: {} }*/
+    var myself = this;
+
+    // PROTECTED
+    this._options = {};
+    this._interfaces = {};
+    this._libraries = {
+      predicates: {
+        tautology: function (value){ return true },
+        isFunction: _.isFunction ,
+        isPositive: function (value){ return (_.isNumber(value) && value > 0); },
+        isObjectOrPropertiesArray : function (value){ 
+          return _.isArray(value) || _.isObject(value);
+        },
+        isObject: _.isObject,
+        isArray: _.isArray
+      },
+      mappers: {
+        identity: _.identity,
+        propertiesObject: function (value) {
+          return (_.isArray(value)) ? D.propertiesArrayToObject(value) : value;
+        }
+      }
+    };
+
+    // PUBLIC
+    this.mixin = function (instance){
+      instance.getOption = this.getOption;
+      instance.setOption = this.setOption;
+    };
+
+    this.init = function (defaults, interfaces, libraries) {
+      var myself = this;
+      this._libraries = $.extend(true, {}, this._libraries, libraries);
+      _.each( interfaces, function (el,key){
+        setInterfaces( key, el );
+      });
+      _.each( defaults, function(el, key) {
+        var ifaces = ( interfaces && interfaces[key] ) || {};
+        myself.setOption(key, el, ifaces);
+      })
+    };
+
+    this.setOption = function (opt, value, interfaces){
+      setInterfaces(opt, interfaces);
+      var reader = getReader(opt),
+          validator = getValidator(opt);
+      if ( validator(value) ){
+        value = reader(value);
+        setValue( opt, value );
+        return true
+      } else {
+        throw new Error( "Invalid Property " + prop.charAt(0).toUpperCase() + prop.slice(1) );
+      }
+    };
+
+    this.getOption = function (opt){
+      var writer = getWriter( opt ),
+          value = getValue(opt);
+      return writer( value );
+    }; 
+
+    // PRIVATE
+    function setInterfaces (opt, interfaces){
+      interfaces = interfaces || {};
+      setReader(opt, interfaces['reader']);
+      setWriter(opt, interfaces['writer']);
+      setValidator(opt, interfaces['validator']);
+    };
+
+    function getReader(opt){ 
+      return get( myself._interfaces, opt, 'reader', myself._libraries.mappers['identity'] 
+    )};
+    function getWriter(opt){
+      return get( myself._interfaces, opt, 'writer', myself._libraries.mappers['identity'] 
+    )};
+    function getValidator(opt){ 
+      return get( myself._interfaces, opt, 'validator', myself._libraries.predicates['tautology'] 
+    )};
+    function getValue(opt){ return get( myself._options, opt, 'value') };
+    
+    // Reader, Writer and Validator work in the same way:
+    // If the value is a function, use it. 
+    // Otherwise, if it is a string and a valid library key, use it.
+    // Otherwise, use a default library function: for readers and writers an indentity map, 
+    //    for validators a predicate that always returns true.
+
+    function setReader(opt, fn){
+      var lib = myself._libraries.mappers;
+      fn = ( _.isFunction(fn) && fn ) || ( _.isString(fn) && lib[fn] ) || getReader(opt) || lib['identity'] ;
+      return set( myself._interfaces , opt, 'reader', fn) 
+    };
+    function setWriter(opt, fn){ 
+      var lib = myself._libraries.mappers;
+      fn = ( _.isFunction(fn) && fn ) || ( _.isString(fn) && lib[fn] ) || getWriter(opt) || lib['identity'] ;
+      return set( myself._interfaces, opt, 'writer', fn) 
+    };
+    function setValidator(opt, fn){ 
+      var lib = myself._libraries.predicates;
+      fn = ( _.isFunction(fn) && fn ) || ( _.isString(fn) && lib[fn] ) || getValidator(opt) || lib['tautology'] ;
+      return set( myself._interfaces, opt, 'validator', fn)
+    };
+    function setValue(opt, value){ return set( myself._options, opt, 'value', value) };
+
+    // Init
+    this.init( config.defaults, config.interfaces, config.libraries);
+
+  }
+
+  // Shared / Static
+  function get ( container, opt, attr, defaultValue ){
+    var val = defaultValue || undefined ;   
+    if ( container && container[opt] && container[opt].hasOwnProperty(attr) ){
+      val = container[opt][attr];
+    }
+    return val
+  }
+  function set (container, opt, attr, value){
+    if (container && opt && attr){
+      container[opt] = container[opt] || {};
+      container[opt][attr] = value ; 
+    }
+  }
+
+  D.OptionsManager = OptionsManager;
+})(Dashboards);
+// OPTIONS MANAGER end
+
+
 // QUERIES begin
 
 Dashboards.getQuery = function (opts) {
@@ -2503,6 +2822,9 @@ Dashboards.getQuery = function (opts) {
     return queryAddIn.getObject();
   }
 };
+
+
+
 
 /*
  * Query STUFF
@@ -2541,6 +2863,9 @@ Query = function( cd, dataAccessId) {
 
   return Dashboards.getQuery(opts);
 };
+
+// QUERIES end
+
 
 
 
