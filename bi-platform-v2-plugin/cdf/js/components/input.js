44d2262fba76291396e4f568576282c60e313fc5
Luis Silva <luis.silva@webdetails.pt>
2013-06-28 09:53:36 +0100

    [Fix] Redmine Issue 1040 - Properties of the MonthPicker component are expecting date objects and getting strings

--- bi-platform-v2-plugin/cdf/js/components/input.js
+++ bi-platform-v2-plugin/cdf/js/components/input.js
@@ -549,7 +549,7 @@ var MonthPickerComponent = BaseComponent.extend({
     });
   },
   getValue : function() {
-    var value = $("#" + this.name).val()
+    var value = $("#" + this.name).val();
 
     var year = value.substring(0,4);
     var month = parseInt(value.substring(5,7) - 1);
@@ -563,26 +563,85 @@ var MonthPickerComponent = BaseComponent.extend({
       Dashboards.processChange(myself.name);
     });
     return value;
-  },
-  getMonthPicker : function(object_name, object_size, initialDate, minDate, maxDate, monthCount) {
+  },parseDate : function(aDateString){
+    //This works assuming the Date comes in this format -> yyyy-mm-dd or yyyy-mm
+    //Date.UTC(year[year after 1900],month[0 to 11],day[1 to 31], hours[0 to 23], min[0 to 59], sec[0 to 59], ms[0 to 999])
+    var parsedDate = null;
+    var yearIndex = 0, monthIndex = 1, dayindex = 2;
+    var split = aDateString.split("-");
+    var year, month, day;
+
+    if(split.length == 3){
+      year = parseInt(split[yearIndex]);
+      month = parseInt(split[monthIndex]);
+      day = parseInt(split[dayindex]);
+      parsedDate = new Date(Date.UTC(year,(month-1),day));
+    }else if(split.length == 2){
+      year = parseInt(split[yearIndex]);
+      month = parseInt(split[monthIndex]);
+      parsedDate = new Date(Date.UTC(year,(month-1)));
+    }
+
+    return parsedDate;
+  },getMonthsAppart : function(aDateOne, aDateTwo){
+    var min, max;
+    if(aDateOne < aDateTwo){
+      min = aDateOne;
+      max = aDateTwo;
+    }else{
+      min = aDateTwo;
+      max = aDateOne;
+    }
+
+    var yearsAppart = (max.getFullYear() - min.getFullYear());
+    var monthsToAdd = yearsAppart * 12;
+    var monthCount = (max.getMonth() - min.getMonth()) + monthsToAdd; //TODO verify this calculation
+    
+    return monthCount;
+  },normalizeDateToCompare : function(dateObject){
+    var normalizedDate = dateObject;
+    normalizedDate.setDate(1);
+    normalizedDate.setHours(0);
+    normalizedDate.setMinutes(0);
+    normalizedDate.setSeconds(0);
+    normalizedDate.setMilliseconds(0);
+
+    return normalizedDate;
+
+  },getMonthPicker : function(object_name, object_size, initialDate, minDate, maxDate, monthCount) {
 
 
     var selectHTML = "<select";
     selectHTML += " id='" + object_name + "'";
 
-    if (minDate == undefined){
+    if(initialDate == undefined || initialDate == null){
+      initialDate = new Date();
+    }
+    if (minDate == undefined || minDate == null){
       minDate = new Date();
       minDate.setYear(1980);
     }
-    if (maxDate == undefined){
+    if (maxDate == undefined || maxDate == null){
       maxDate = new Date();
       maxDate.setYear(2060);
     }
 
+    //if any of the dates comes in string format this will parse them
+    if(typeof initialDate === "string"){
+      initialDate = this.parseDate(initialDate);
+    }
+    if(typeof minDate === "string"){
+      minDate = this.parseDate(minDate);
+    }
+    if(typeof maxDate === "string"){
+      maxDate = this.parseDate(maxDate);
+    }
+
     // if monthCount is not defined we'll use everything between max and mindate
+    var monthCountUndefined = false;
     if(monthCount == undefined || monthCount == 0) {
-      var monthsToAdd = (maxDate.getFullYear() - minDate.getFullYear())*12;
-      monthCount = (maxDate.getMonth() - minDate.getMonth()) + monthsToAdd;
+      monthCount = this.getMonthsAppart(minDate,maxDate);
+      monthCountUndefined = true;
     }
 
     //set size
@@ -592,14 +651,27 @@ var MonthPickerComponent = BaseComponent.extend({
     selectHTML += '>';
 
     var currentDate = new Date(+initialDate);
-    currentDate.setMonth(currentDate.getMonth()- monthCount/2 - 1);
+
+    /*
+    * This block is to make sure the months are compared equally. A millisecond can ruin the comparation.
+    */
+
+      if(monthCountUndefined == true){
+        currentDate.setMonth(currentDate.getMonth() - (this.getMonthsAppart(minDate,currentDate)) - 1);
+      }else{
+        currentDate.setMonth(currentDate.getMonth() - (monthCount/2) - 1);
+      }
+    currentDate = this.normalizeDateToCompare(currentDate);
+    var normalizedMinDate = this.normalizeDateToCompare(minDate);
+    var normalizedMaxDate = this.normalizeDateToCompare(maxDate);
 
     for(var i= 0; i <= monthCount; i++){
-
+      
       currentDate.setMonth(currentDate.getMonth() + 1);
-      if(currentDate >= minDate && currentDate <= maxDate)
+
+      if(currentDate >= normalizedMinDate && currentDate <= normalizedMaxDate)
       {
-        selectHTML += "<option value = '" + currentDate.getFullYear() + "-" + this.zeroPad(currentDate.getMonth()+1,2) + "' ";
+        selectHTML += "<option value = '" + currentDate.getFullYear() + "-" + this.zeroPad((currentDate.getMonth()+1),2) + "' ";
 
         if(currentDate.getFullYear() == initialDate.getFullYear() && currentDate.getMonth() == initialDate.getMonth()){
           selectHTML += "selected='selected'"
