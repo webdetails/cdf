9c90a48d043292bdf226a2842bbba45440293b04
Duarte Cunha Leão <duarte.leao@gmail.com>
2013-07-22 22:06:36 +0100

    [FIX] (cdf/Dashboards requirejs) Dependency order was not correct. Specifically, jquery.blockUI, wasn't loaded before Dashboards.js, which sometimes generated a load time error. Also, did some cleanup to the dependency declarations.

--- bi-platform-v2-plugin/cdf/js/cdf-require-js-cfg.js
+++ bi-platform-v2-plugin/cdf/js/cdf-require-js-cfg.js
@@ -1,5 +1,5 @@
 requireCfg['paths']['cdf'] = CONTEXT_PATH+'content/pentaho-cdf/js';
-requireCfg['shim']['cdf/cdf-module'] = ['cdf/Dashboards',
+requireCfg['shim']['cdf/cdf-module'] = [
 	'cdf/jquery.ui',
 	'cdf/jquery-impromptu.3.1',
 	'cdf/jquery-ui-datepicker-i18n',
@@ -12,27 +12,12 @@ requireCfg['shim']['cdf/cdf-module'] = ['cdf/Dashboards',
 	'cdf/jquery.positionBy',
 	'cdf/jquery.sparkline',
 	'cdf/jquery.tooltip',
-	'cdf/simile/ajax/simile-ajax-api',
+	
 	'cdf/simile/ajax/scripts/json',
 	'cdf/json',
-    'cdf/lib/shims',
-    'cdf/lib/CCC/pvc-d1.0',
-    'cdf/lib/CCC/protovis', 
-    'cdf/lib/CCC/tipsy', 
-    'cdf/lib/CCC/jquery.tipsy', 
-    'cdf/lib/CCC/def',    
-	'cdf/backbone',
-	'cdf/underscore',
-	'cdf/mustache',
-	'cdf/components/ccc',
-	'cdf/components/core',
-    'cdf/components/input'	,
-    'cdf/components/jfreechart',    
-    'cdf/components/maps',
-    'cdf/components/navigation',
-    'cdf/components/pentaho',
-    'cdf/components/simpleautocomplete',
-	'cdf/components/table'];
+
+	'cdf/CoreComponents'
+];
 
 
 requireCfg['shim']['cdf/CoreComponents'] = [
@@ -47,45 +32,44 @@ requireCfg['shim']['cdf/CoreComponents'] = [
 	'cdf/components/table'
 ];
 
-
-
 requireCfg['shim']['cdf/Dashboards'] = [
     'cdf/Base',
     'cdf/underscore',
     'cdf/backbone',
     'cdf/mustache', 
-    'cdf/lib/shims'    
+    'cdf/lib/shims',
+    'cdf/jquery.blockUI'
 ];
 
-
 requireCfg['shim']['cdf/backbone'] = ['cdf/underscore'];
-requireCfg['shim']['cdf/lib/CCC/pvc-d1.0'] = ['cdf/lib/CCC/protovis', 'cdf/lib/CCC/tipsy', 'cdf/lib/CCC/jquery.tipsy', 'cdf/lib/CCC/def'];
 
-requireCfg['shim']['cdf/components/core'] = ['cdf/Dashboards'];
-requireCfg['shim']['cdf/components/ccc'] = ['cdf/components/core', 'cdf/lib/CCC/pvc-d1.0'];
-requireCfg['shim']['cdf/components/input'] = ['cdf/components/core'];
+requireCfg['shim']['cdf/lib/CCC/pvc-d1.0']     = ['cdf/lib/CCC/protovis', 'cdf/lib/CCC/tipsy', 'cdf/lib/CCC/def'];
+requireCfg['shim']['cdf/lib/CCC/tipsy']        = ['cdf/lib/CCC/protovis', 'cdf/lib/CCC/jquery.tipsy'];
+requireCfg['shim']['cdf/lib/CCC/jquery.tipsy'] = ['cdf/jquery'];
+
+requireCfg['shim']['cdf/components/core']       = ['cdf/Dashboards'];
+requireCfg['shim']['cdf/components/ccc']        = ['cdf/components/core', 'cdf/lib/CCC/pvc-d1.0'];
+requireCfg['shim']['cdf/components/input']      = ['cdf/components/core'];
 requireCfg['shim']['cdf/components/jfreechart'] = ['cdf/components/core'];
-requireCfg['shim']['cdf/components/maps'] = ['cdf/components/core'];
+requireCfg['shim']['cdf/components/maps']       = ['cdf/components/core'];
 requireCfg['shim']['cdf/components/navigation'] = ['cdf/components/core'];
-requireCfg['shim']['cdf/components/pentaho'] = ['cdf/components/core'];
+requireCfg['shim']['cdf/components/pentaho']    = ['cdf/components/core'];
 requireCfg['shim']['cdf/components/simpleautocomplete'] = ['cdf/components/core'];
-requireCfg['shim']['cdf/components/table'] = ['cdf/components/core'];
+requireCfg['shim']['cdf/components/table']      = ['cdf/components/core'];
 
-requireCfg['shim']['cdf/jquery.ui'] = ['cdf/jquery'];
-requireCfg['shim']['cdf/jquery-impromptu.3.1'] = ['cdf/jquery'];
+requireCfg['shim']['cdf/jquery.ui']              = ['cdf/jquery'];
+requireCfg['shim']['cdf/jquery-impromptu.3.1']   = ['cdf/jquery'];
 requireCfg['shim']['cdf/jquery-ui-datepicker-i18n'] = ['cdf/jquery.ui'];
-requireCfg['shim']['cdf/jquery.bgiframe'] = ['cdf/jquery'];
-requireCfg['shim']['cdf/jquery.blockUI'] = ['cdf/jquery'];
-requireCfg['shim']['cdf/jquery.corner'] = ['cdf/jquery'];
-requireCfg['shim']['cdf/jquery.eventstack'] = ['cdf/jquery'];
+requireCfg['shim']['cdf/jquery.bgiframe']        = ['cdf/jquery'];
+requireCfg['shim']['cdf/jquery.blockUI']         = ['cdf/jquery'];
+requireCfg['shim']['cdf/jquery.corner']          = ['cdf/jquery'];
+requireCfg['shim']['cdf/jquery.eventstack']      = ['cdf/jquery'];
 requireCfg['shim']['cdf/jquery.i18n.properties'] = ['cdf/jquery'];
-requireCfg['shim']['cdf/jquery.jdMenu'] = ['cdf/jquery'];
-requireCfg['shim']['cdf/jquery.positionBy'] = ['cdf/jquery'];
-requireCfg['shim']['cdf/jquery.sparkline'] = ['cdf/jquery'];
-requireCfg['shim']['cdf/jquery.tooltip'] = ['cdf/jquery'];
+requireCfg['shim']['cdf/jquery.jdMenu']          = ['cdf/jquery'];
+requireCfg['shim']['cdf/jquery.positionBy']      = ['cdf/jquery'];
+requireCfg['shim']['cdf/jquery.sparkline']       = ['cdf/jquery'];
+requireCfg['shim']['cdf/jquery.tooltip']         = ['cdf/jquery'];
 
 requireCfg['shim']['cdf/simile/ajax/scripts/json'] = ['cdf/simile/ajax/simile-ajax-api'];
 
 requireCfg['shim']['cdf/json'] = ['cdf/simile/ajax/simile-ajax-api'];
-
-
