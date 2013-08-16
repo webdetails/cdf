db39b59ccb113086e2b3bcbce89e57e2d33865bb
Pedro Teixeira <pedrofvteixeira@gmail.com>
2013-08-16 12:37:36 +0100

    tested new rest calls against 5.0 plugin-samples (normal and migrated ones). Moved constants to CDFConstants class. Moved general helper methods to Util class; Implemented new executeAction that calls 5.0 XActionUtil

--- bi-platform-v2-plugin/src/org/pentaho/cdf/xactions/ActionEngine.java
+++ bi-platform-v2-plugin/src/org/pentaho/cdf/xactions/ActionEngine.java
@@ -2,12 +2,24 @@ package org.pentaho.cdf.xactions;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.pentaho.cdf.utils.Util;
+import org.pentaho.platform.api.engine.IMimeTypeListener;
 import org.pentaho.platform.api.engine.IPentahoSession;
 import org.pentaho.platform.api.engine.IRuntimeContext;
 import org.pentaho.platform.api.engine.ISolutionEngine;
+import org.pentaho.platform.api.repository2.unified.IUnifiedRepository;
+import org.pentaho.platform.api.repository2.unified.RepositoryFile;
+import org.pentaho.platform.engine.core.system.PentahoSystem;
 import org.pentaho.platform.engine.services.solution.SolutionHelper;
+import org.pentaho.platform.util.messages.LocaleHelper;
+import org.pentaho.platform.web.http.api.resources.XactionUtil;
+
 import java.util.HashMap;
+import java.io.IOException;
 import java.io.OutputStream;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class ActionEngine {
 
@@ -25,11 +37,36 @@ public class ActionEngine {
     logger.info("Creating ActionEngine instance");
   }
 
+  @Deprecated
   public boolean executeAction(String resource, IPentahoSession userSession, OutputStream out, HashMap<String, String> params){
     ISolutionEngine engine = SolutionHelper.execute("executeAction", userSession, resource, params, out);
     int status = engine.getExecutionContext().getStatus();
 
     return status == IRuntimeContext.RUNTIME_STATUS_SUCCESS;
   }
-
+  
+  public boolean executeAction(String path, String contentType, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, IPentahoSession userSession, HashMap<String, String> params, IMimeTypeListener mimeTypeListener) throws IOException{
+	  
+	  boolean success = false;
+	  
+	  try{
+	  
+		  IUnifiedRepository unifiedRepository = PentahoSystem.get(IUnifiedRepository.class, null);
+	      RepositoryFile file = unifiedRepository.getFile(path);
+		  
+		  String buffer = XactionUtil.execute(contentType, file, httpServletRequest, httpServletResponse, userSession, mimeTypeListener);
+		  
+	      if (!Util.isEmpty(buffer)) {
+	    	  httpServletResponse.getOutputStream().write(buffer.getBytes(LocaleHelper.getSystemEncoding()));
+	      }
+	
+	      success = true;
+      
+	  } catch(Throwable t){
+		logger.error(t);
+		httpServletResponse.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, t.getMessage());
+	  }
+	  
+	  return success;
+  }
 }
