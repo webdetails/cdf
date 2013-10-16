2c538e1fa2602de690e5763327c011d52765d8e9
Duarte Cunha Leão <duarte.leao@gmail.com>
2013-10-16 22:08:23 +0100

    [FIX] (CCC CDF compatVersion) Registered the compatVersion.js file in the blueprint style resources. Changed the compatVersion.js file to use pen.define. This sets the default CCC compatVersion to 1, but doesn't yet address the lack of pvc, def and pv as global JS namespaces. So, still, some more complex charts, or related dashboard resource scripts, are going to fail.

--- bi-platform-v2-plugin/cdf/js/lib/CCC/compatVersion.js
+++ bi-platform-v2-plugin/cdf/js/lib/CCC/compatVersion.js
@@ -1,2 +1,4 @@
-// Set the default compatibility version to CCC v1.
-pvc.defaultCompatVersion(1);
+pen.define("cdf/lib/CCC/compatVersion", ["cdf/lib/CCC/pvc-d1.0"], function(pvc) {
+	// Set the default compatibility version to CCC v1.
+	pvc.defaultCompatVersion(1);
+});
