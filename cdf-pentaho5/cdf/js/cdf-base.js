505c598b6c024f1b8e0dcaf0e8f8d5c0cc7b265c
André Jorge <andre.jorge@webdetails.pt>
2014-03-19 14:35:07 +0000

    [CDF-206] Refactor CDF into cdf-core, cdf-pentaho, cdf-pentaho5 	- cleaned unecessary comments and debugging echos in build.xml 	- edited CDF_RESOURCES_BASE_URL in build.properties 	- legacy fallback ContextEngine also includes solution in Dashboard.context

--- cdf-pentaho5/cdf/js/cdf-base.js
+++ cdf-pentaho5/cdf/js/cdf-base.js
@@ -57,7 +57,7 @@ var Endpoints = {
 
   getExport: function () { return Endpoints.getCdfBase() + "/Export"; },
 
-  getResource: function() { return Endpoints.getCdfBase() + "/resource"; },
+  getResource: function() { return Endpoints.getCdfBase() + "/resources"; },
 
   getCdfXaction: function( path, action, solution ) { 
     return Endpoints.getViewAction() + "?path=" + Helper.getFullPath( path, action ) + "&" + Helper.getTimestamp(); 
