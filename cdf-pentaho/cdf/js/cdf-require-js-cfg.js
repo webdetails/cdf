6f690f9bbd10414bf5b476b14bf35594c791d586
David Santos <david.santos@webdetails.pt>
2014-08-01 17:35:00 +0100

    [CDF-341]: fixed uncaught TypeError while opening PrptComponent

--- cdf-pentaho/cdf/js/cdf-require-js-cfg.js
+++ cdf-pentaho/cdf/js/cdf-require-js-cfg.js
@@ -114,8 +114,8 @@ requireCfg['shim']['cdf/backbone'] 				= ['cdf/underscore'];
 requireCfg['shim']['cdf/lib/CCC/compatVersion'] = ['cdf/lib/CCC/pvc-d1.0'];
 requireCfg['shim']['cdf/lib/CCC/pvc-d1.0'] 		= ['cdf/lib/CCC/protovis', 'cdf/lib/CCC/tipsy', 'cdf/lib/CCC/jquery.tipsy', 'cdf/lib/CCC/def'];
 
-requireCfg['shim']['cdf/lib/CCC/tipsy'] 		= ['cdf/lib/CCC/protovis'];
-requireCfg['shim']['cdf/lib/CCC/jquery.tipsy'] 	= ['cdf/lib/CCC/tipsy'];
+requireCfg['shim']['cdf/lib/CCC/tipsy'] 		= ['cdf/lib/CCC/protovis', 'cdf/lib/CCC/jquery.tipsy'];
+requireCfg['shim']['cdf/lib/CCC/jquery.tipsy'] 	= ['cdf/jquery'];
 
 requireCfg['shim']['cdf/components/core'] 		= ['cdf/Dashboards'];
 requireCfg['shim']['cdf/components/ccc'] 		= ['cdf/components/core', 'cdf/lib/CCC/pvc-d1.0'];
