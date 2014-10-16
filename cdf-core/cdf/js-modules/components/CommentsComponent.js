290857897c427bc7848639e7f6630bcb50de687c
Andre <andre.jorge@webdetails.pt>
2014-10-16 13:00:50 +0100

    [CDF-332] - Split SchedulePrptComponent, TrafficComponent, JFreeChartComponent and CommentsComponent CSS rules into single files, allowing samples to run in legacy, debug and production (compressed) mode [CDF-402] - Fixed hardcoded image paths in cdf.css used by legacy samples [CDF-403] - Moved SchedulePrptComponent, TrafficComponent, JFreeChartComponent and blockUI images into appropriate locations, allowing samples to run in legacy, debug and production (compressed) mode

--- cdf-core/cdf/js-modules/components/CommentsComponent.js
+++ cdf-core/cdf/js-modules/components/CommentsComponent.js
@@ -11,7 +11,7 @@
  * the license for the specific language governing your rights and limitations.
  */
 
-define(['./CommentsComponent.ext', '../lib/mustache', "../lib/underscore", "../lib/backbone", './BaseComponent', '../Logger', '../lib/jquery'],
+define(['./CommentsComponent.ext', '../lib/mustache', "../lib/underscore", "../lib/backbone", './BaseComponent', '../Logger', '../lib/jquery', 'css!./CommentsComponent'],
   function(CommentsComponentExt, Mustache, _, Backbone, BaseComponent, Logger, $) {
 
   var CommentsComponent = BaseComponent.extend({
