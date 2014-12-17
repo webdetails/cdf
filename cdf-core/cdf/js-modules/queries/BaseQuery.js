e089015ce63e9d14b737bc7e6accbf55447d8180
Joao Velasques <joao.velasques@webdetails.pt>
2014-12-17 16:23:40 +0000

    [CDF-441] - Fixing previous solution

--- cdf-core/cdf/js-modules/queries/BaseQuery.js
+++ cdf-core/cdf/js-modules/queries/BaseQuery.js
@@ -114,7 +114,8 @@
         error: this.getErrorHandler(errorCallback)
       });
 
-      if(!$.ajaxSettings.async && settings.xhrFields && settings.xhrFields.withCredentials) {
+      var async = settings.async == null ?  $.ajaxSettings.async : settings.async;
+      if ( !async && settings.xhrFields && settings.xhrFields.withCredentials) {
         Logger.log("Cross-domain requests are deprecated for synchronous operations.");
         delete settings.xhrFields.withCredentials;
       }
