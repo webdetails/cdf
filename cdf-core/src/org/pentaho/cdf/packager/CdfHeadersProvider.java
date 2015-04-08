d376dcb112c540e7b43014048a5f0e9939803a2e
João Velasques <joao.velasques@webdetails.pt>
2015-04-08 14:43:00 +0100

    [CDF-328] - Adding unit test for CdfHeadersProvider

--- cdf-core/src/org/pentaho/cdf/packager/CdfHeadersProvider.java
+++ cdf-core/src/org/pentaho/cdf/packager/CdfHeadersProvider.java
@@ -224,7 +224,7 @@ public class CdfHeadersProvider implements ICdfHeadersProvider {
     }
   }
 
-  private void appendDependencies( StringBuilder deps, DependenciesPackage pkg, boolean minify,
+  protected void appendDependencies( StringBuilder deps, DependenciesPackage pkg, boolean minify,
                                    String absRoot, final ArrayList<String> files ) {
     if ( absRoot != null ) {
       StringFilter filter = new AbsolutizingStringFilter( absRoot, pkg.getDefaultFilter() );
@@ -234,7 +234,7 @@ public class CdfHeadersProvider implements ICdfHeadersProvider {
     }
   }
 
-  private void appendDependencies( StringBuilder deps, DependenciesPackage pkg, boolean minify, String absRoot ) {
+  protected void appendDependencies( StringBuilder deps, DependenciesPackage pkg, boolean minify, String absRoot ) {
     if ( absRoot != null ) {
       StringFilter filter = new AbsolutizingStringFilter( absRoot, pkg.getDefaultFilter() );
       deps.append( pkg.getDependencies( filter, minify ) );
@@ -388,11 +388,11 @@ public class CdfHeadersProvider implements ICdfHeadersProvider {
         .toArray( new String[fileNames.size()] ) );
   }
 
-  private IUrlProvider getUrlProvider() {
+  protected IUrlProvider getUrlProvider() {
     return PluginEnvironment.env().getUrlProvider();
   }
 
-  private IContentAccessFactory getContentAccess() {
+  protected IContentAccessFactory getContentAccess() {
     return PluginEnvironment.repository();
   }
 
