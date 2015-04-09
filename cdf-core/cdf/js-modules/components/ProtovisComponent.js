2f9da44dadec2a10b75b7d908593123e07b8b0e1
João Velasques <joao.velasques@webdetails.pt>
2015-04-09 16:43:20 +0100

    [CDF-525] - Charts don't render in require dashboards in IE8

--- cdf-core/cdf/js-modules/components/ProtovisComponent.js
+++ cdf-core/cdf/js-modules/components/ProtovisComponent.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright 2002 - 2014 Webdetails, a Pentaho company.  All rights reserved.
+ * Copyright 2002 - 2015 Webdetails, a Pentaho company.  All rights reserved.
  *
  * This software was developed by Webdetails and is provided under the terms
  * of the Mozilla Public License, Version 2.0, or any later version. You may not use
@@ -13,9 +13,10 @@
 
 define([
     './ChartComponent',
+    '../lib/jquery',
     '../lib/CCC/protovis',
-    '../lib/jquery'],
-  function(ChartComponent, pv, $) {
+    '../lib/CCC/protovis-msie'],
+  function(ChartComponent, $, pv) {
 
   var ProtovisComponent =  ChartComponent.extend({
 
