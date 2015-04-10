5eb2832ddcf6be685a851ac7a54844b0b59a415b
Andre <andre.jorge@webdetails.pt>
2015-04-10 18:36:04 +0100

    [CLEANUP] 	- Avoid DISCONNECTED messages while running tests using PhantomJS, for 4.x and 5 tests also

--- cdf-pentaho5/config/karma.ci.conf.js
+++ cdf-pentaho5/config/karma.ci.conf.js
@@ -91,7 +91,11 @@ module.exports = function(config) {
     // If browser does not capture in given timeout [ms], kill it
     captureTimeout: 60000,
 
-    //browserNoActivityTimeout: 1200000,
+    // to avoid DISCONNECTED messages
+    // see https://github.com/karma-runner/karma/issues/598
+    browserDisconnectTimeout : 10000, // default 2000
+    browserDisconnectTolerance : 1, // default 0
+    browserNoActivityTimeout : 60000, //default 10000
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
@@ -103,9 +107,7 @@ module.exports = function(config) {
       'karma-junit-reporter',
       'karma-html-reporter',
       'karma-coverage',
-      'karma-phantomjs-launcher',
-      'karma-chrome-launcher',
-      'karma-firefox-launcher'
+      'karma-phantomjs-launcher'
     ]
   });
 };
