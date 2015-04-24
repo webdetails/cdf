c2d31dedda6c2aeceb0e7571ca2aaaa545991a8e
Andre <andre.jorge@webdetails.pt>
2015-04-24 18:51:52 +0100

    [CDF-328] - Add support for Require.js in CDF 	- fixed reference to dashboard.getErrorObj and added Logger

--- cdf-core/cdf/js-modules/queries/LegacyQuery.js
+++ cdf-core/cdf/js-modules/queries/LegacyQuery.js
@@ -1,13 +1,13 @@
 /*!
- * Copyright 2002 - 2014 Webdetails, a Pentaho company.  All rights reserved.
+ * Copyright 2002 - 2015 Webdetails, a Pentaho company. All rights reserved.
  *
  * This software was developed by Webdetails and is provided under the terms
  * of the Mozilla Public License, Version 2.0, or any later version. You may not use
  * this file except in compliance with the license. If you need a copy of the license,
- * please go to  http://mozilla.org/MPL/2.0/. The Initial Developer is Webdetails.
+ * please go to http://mozilla.org/MPL/2.0/. The Initial Developer is Webdetails.
  *
  * Software distributed under the Mozilla Public License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. Please refer to
  * the license for the specific language governing your rights and limitations.
  */
 
@@ -26,27 +26,27 @@
  * @deprecated
  */
 
-define(['../components/XactionComponent.ext', './BaseQuery', '../dashboard/Dashboard.query', 'amd!../lib/underscore', '../lib/jquery', '../dashboard/Utils'],
-  function(XactionComponentExt, BaseQuery, Dashboard, _, $, Utils) {
+define(['../Logger', '../components/XactionComponent.ext', './BaseQuery', '../dashboard/Dashboard.query', 'amd!../lib/underscore', '../lib/jquery', '../dashboard/Utils'],
+  function(Logger, XactionComponentExt, BaseQuery, Dashboard, _, $, Utils) {
 
   function makeMetadataElement(idx, name, type) {
-    return { "colIndex" : idx || 0, "colType" : type || "String" , "colName" : name || "Name" };
+    return {"colIndex": idx || 0, "colType": type || "String", "colName": name || "Name" };
   }
 
   var legacyOpts = {
     name: "legacy",
     label: "Legacy",
     defaults: {
-      url: XactionComponentExt.getCdfXaction("pentaho-cdf/actions" , "jtable.xaction"),
+      url: XactionComponentExt.getCdfXaction("pentaho-cdf/actions", "jtable.xaction"),
       queryDef: {}
     },
     interfaces:{
       lastResultSet:{
         reader: function(json) {
           json = eval("(" + json + ")");
-          var result = { metadata: [ makeMetadataElement(0)] , resultset:json.values || [] };
-          _.each(json.metadata , function(el, idx) {
-            return result.metadata.push( makeMetadataElement(idx+1, el) );
+          var result = {metadata: [makeMetadataElement(0)], resultset: json.values || []};
+          _.each(json.metadata, function(el, idx) {
+            return result.metadata.push( makeMetadataElement(idx + 1, el) );
           });
           return result
         }
@@ -75,20 +75,20 @@ define(['../components/XactionComponent.ext', './BaseQuery', '../dashboard/Dashb
       var myself = this;
       return function(json) {
         try {
-          myself.setOption('lastResultSet' , json);
+          myself.setOption('lastResultSet', json);
         } catch(e) {
-          if(this.async){
+          if(this.async) {
             // async + legacy errors while parsing json response aren't caught
-            var msg = Dashboard.getErrorObj('COMPONENT_ERROR').msg + ":" + e.message;
-            Dashboard.error(msg);
-            json = {"metadata":[msg],"values":[]};
+            var msg = myself.dashboard.getErrorObj('COMPONENT_ERROR').msg + ":" + e.message;
+            Logger.error(msg);
+            json = {"metadata": [msg], "values": []};
           } else {
             //exceptions while parsing json response are
             //already being caught+handled in updateLifecyle()
             throw e;
           }
         }
-        var clone = $.extend(true,{}, myself.getOption('lastResultSet'));
+        var clone = $.extend(true, {}, myself.getOption('lastResultSet'));
         callback(clone);
       }
     },
@@ -107,11 +107,11 @@ define(['../components/XactionComponent.ext', './BaseQuery', '../dashboard/Dashb
     }
 
   };
-  Dashboard.registerGlobalQuery( "legacy", legacyOpts );
+  Dashboard.registerGlobalQuery("legacy", legacyOpts);
 
   // TODO: Temporary until CDE knows how to write queryTypes definitions, with all these old queries
   // falling under the 'legacy' umbrella.
-  Dashboard.registerGlobalQuery( "mdx", legacyOpts );
-  Dashboard.registerGlobalQuery( "sql", legacyOpts );
+  Dashboard.registerGlobalQuery("mdx", legacyOpts);
+  Dashboard.registerGlobalQuery("sql", legacyOpts);
 
- });
+});
