794b16aa4d47e823ebafef6de01721c113e09371
David Santos <david.santos@webdetails.pt>
2015-04-30 14:35:07 +0100

    [CDF-531] updated autoComplete component to use jquery.ui autocomplete

--- cdf-core/cdf/js-modules/components/AutocompleteBoxComponent.js
+++ cdf-core/cdf/js-modules/components/AutocompleteBoxComponent.js
@@ -11,13 +11,13 @@
  * the license for the specific language governing your rights and limitations.
  */
 
-define(['./QueryComponent', './BaseComponent', '../lib/jquery', 'amd!../lib/jquery.ui.autobox.ext'],
-  function(QueryComponent, BaseComponent, $) {
+define(['./QueryComponent', './BaseComponent', 'amd!../lib/underscore', '../lib/jquery'],
+  function(QueryComponent, BaseComponent, _, $) {
 
   var AutocompleteBoxComponent = BaseComponent.extend({
 
-    searchedWord : '',
     result: [],
+    selectedValues: [],
 
     /**
      *
@@ -25,10 +25,9 @@ define(['./QueryComponent', './BaseComponent', '../lib/jquery', 'amd!../lib/jque
      * @private
      */
     _queryServer : function(searchString) {
-
       if(!this.parameters) {
         this.parameters = []
-      };
+      }
 
       if(this.searchParam) {
         this.parameters = [ [this.searchParam, this._getInnerParameterName()] ];
@@ -61,101 +60,245 @@ define(['./QueryComponent', './BaseComponent', '../lib/jquery', 'amd!../lib/jque
 
     /**
      *
+     * @private
+     */
+    _setInitialValue: function() {
+      var param = this.parameter;
+      var initialValue = null;
+
+      if(param) {
+        initialValue = this.dashboard.getParameterValue(param);
+      }
+
+      if(initialValue != null && _.isArray(initialValue)) {
+        for(var i = 0, L = initialValue.length; i < L; i++) {
+          this._selectValue(initialValue[i]);
+        }
+      }
+    },
+
+    /**
+     *
      */
     update : function() {
+      this.placeholder().empty();
 
       var myself = this;
-
-      myself.placeholder().empty();
-
-      var initialValue = null;
-      if(myself.parameter) {
-        initialValue = myself.dashboard.getParameterValue(myself.parameter);
-      }
+      var isMultiple = this.selectMulti || false;
+      var options = this._getOptions();
 
       //init parameter
-      if(!myself.dashboard.getParameterValue(myself._getInnerParameterName())) {
-        myself.dashboard.setParameter(myself._getInnerParameterName(), '');
+      if(!this.dashboard.getParameterValue(this._getInnerParameterName())) {
+        this.dashboard.setParameter(this._getInnerParameterName(), '');
       }
 
-      var processChange = ((myself.processChange == undefined)
-        ? function(objName) {
-            myself.dashboard.processChange(objName);
-          }
-        : function(objName) {
-          myself.processChange();
-        }
-      );
-      var processElementChange = ((myself.processElementChange == true)
-        ? function(value) {
-            myself.dashboard.fireChange(myself.parameter,value);
-          }
-        : undefined
-      );
+      this.textbox = $('<input class="autocomplete-input">');
+      var autoComplete = $('<div class="autocomplete-container">');
 
-      if(myself.minTextLength == undefined) {
-        myself.minTextLength = 0;
+      if(isMultiple) {
+        var title = this.tooltipMessage || "Click it to Apply";
+        var apply = $('<input type="button" class="autocomplete-input-apply" style="display: none" title="' + title + '" value="S"/>')
+            .click(function() {
+              myself._endSearch();
+            });
+        autoComplete.append(apply)
       }
 
-      var opt = {
-        list: function() {
-          var val = myself._getTextBoxValue();
-          if(val.length >= myself.minTextLength
-            && !(val == '' //nothing to search
-              || val == myself.searchedWord
-              || ((myself.queryInfo != null && myself.result.length == myself.queryInfo.totalRows)
-                //has all results
-                && myself.searchedWord != ''
-                && ((myself.matchType == "fromStart")
-                    ? val.indexOf(myself.searchedWord) == 0
-                    : val.indexOf(myself.searchedWord) > -1)))) //searchable in local results
-          {
-            myself._queryServer(val);
-            myself.searchedWord = val;
+      autoComplete
+          .append(this.textbox)
+          .append('<ul class="list-data-selection">')
+          .appendTo(this.placeholder());
+
+      this.textbox.autocomplete(options);
+
+      $('.autocomplete-container .ui-autocomplete').off('menuselect');
+      this.textbox.data('ui-autocomplete')._renderItem = function(ul, item) {
+        var listItem = $('<li class="list-item">');
+
+        var content = $('<a>' + (isMultiple ? '<input type="checkbox"/>' : '') + item.label + '</a>').click(function(event) {
+          var checkbox = $(this).find('input');
+          if($(event.srcElement).is('a')) {
+            checkbox.prop('checked', !checkbox.is(':checked'))
           }
-          var list = [];
-          for(p in myself.result) if (myself.result.hasOwnProperty(p)) {
-            var obj = {};
-            obj.text = myself.result[p][0];
-            list.push(obj);
+          if(!isMultiple) {
+            myself._selectValue(item.label);
+            myself._endSearch();
+          } else if(checkbox.is(':checked')) {
+            myself._selectValue(item.label);
+          } else {
+            myself._removeValue(item.label);
           }
-          return list;
-        },
-        matchType: myself.matchType == undefined ? "fromStart" : myself.matchType, /*fromStart,all*/
-        processElementChange:  processElementChange,
-        processChange: function(obj,value) {
-          obj.value = value;
-          processChange(obj.name);
-        },
-        multiSelection: myself.selectMulti == undefined ? false : myself.selectMulti,
-        checkValue: myself.checkValue == undefined ? true : myself.checkValue,
-        minTextLength: myself.minTextLength == undefined ? 0 : myself.minTextLength,
-        scrollHeight: myself.scrollHeight,
-        applyButton: myself.showApplyButton == undefined ? true : myself.showApplyButton,
-        tooltipMessage: myself.tooltipMessage == undefined ? "Click it to Apply" : myself.tooltipMessage,
-        addTextElements: myself.addTextElements == undefined ? true : myself.addTextElements,
-        externalApplyButtonId: myself.externalApplyButtonId,
-        //    selectedValues: initialValue,
-        parent: myself
+        });
+
+        content.appendTo(listItem);
+        return listItem.appendTo(ul);
       };
 
-      myself.autoBoxOpt = myself.placeholder().autobox(opt);
+      //if defined and it exists bind a click event to this button
+      $('#' + this.externalApplyButtonId).click(function() {
+        myself._endSearch();
+      });
 
-      //setInitialValue
-      myself.autoBoxOpt.setInitialValue(myself.htmlObject, initialValue, myself.name);
-
-      myself.textbox = myself.placeholder('input');
-
-      myself._doAutoFocus();
+      this._setInitialValue();
     },
 
     /**
      *
      * @returns {*}
+     */
+    getValue : function() {
+      return this.value;
+    },
+
+    /**
+     *
+     * @returns {{appendTo: string, minLength: (AutocompleteBoxComponent.minTextLength|*|number), source: Function, focus: Function, open: Function, close: Function}}
      * @private
      */
-    _getValue : function() {
-      return this.value;
+    _getOptions: function() {
+      var myself = this;
+
+      var processChange = this.processChange == null ?
+          function() {
+            var object = _.extend({}, myself);
+            object.value = myself.selectedValues;
+            myself.dashboard.processChange(object.name);
+          } :
+          function() {
+            myself.processChange();
+          };
+
+      var options = {
+        appendTo: '.autocomplete-container',
+        minLength: this.minTextLength || 0,
+        source: function(search, callback) {
+          myself._search(search, callback);
+        },
+
+        focus: function(event, ui) {
+          event.preventDefault();
+        },
+
+        open: function(event, ui) {
+          var scroll = myself.scrollHeight || 0;
+
+          if(scroll > 0) {
+            $('.autocomplete-container .ui-autocomplete').css({'max-height': scroll + 'px', 'overflow-y': 'auto'});
+          }
+
+          myself._filterData();
+        },
+
+        close: function(event, ui) {
+          var container = $('.autocomplete-container');
+
+          container.removeClass('show-apply-button');
+          container.find('.autocomplete-input-apply').hide();
+
+          myself.textbox.val('');
+          processChange();
+        }
+      };
+
+      return options;
+    },
+
+    /**
+     *
+     * @param label
+     * @private
+     */
+    _selectValue: function(label) {
+      var myself = this;
+      var addTextElements = this.addTextElements != null ? this.addTextElements : true;
+      var showApplyButton = this.showApplyButton != null ? this.showApplyButton : true;
+      var list = $('.autocomplete-container .list-data-selection');
+      var listItem = $('<li id="' + label + '"><input type="button" class="close-button" value="x"/>' + label + '</li>');
+
+      if(!this.selectMulti) {
+        list.empty();
+        this.selectedValues = [];
+      } else if(showApplyButton) {
+        $('.autocomplete-container').addClass('show-apply-button');
+        $('.autocomplete-input-apply').show();
+      }
+
+      listItem.find('input').click(function() {
+        myself._removeValue(label);
+      });
+
+      if(addTextElements) {
+        listItem.appendTo(list);
+      }
+      this.selectedValues.push(label);
+    },
+
+    /**
+     *
+     * @param id
+     * @private
+     */
+    _removeValue: function(id) {
+      this.selectedValues = _.without(this.selectedValues, id);
+      $('.autocomplete-container .list-data-selection li[id="' + id + '"]').remove();
+    },
+
+    _filterData: function() {
+      var menu = $('.autocomplete-container .ui-autocomplete');
+      var data = this.selectedValues || [];
+      var addTextElements = this.addTextElements != null ? this.addTextElements : true;
+
+      if(data.length > 0) {
+        menu.find('li').each(function () {
+          var $this = $(this);
+          var label = $this.text();
+          if(data.indexOf(label) > -1) {
+            if(addTextElements) {
+              $this.remove();
+            } else {
+              $this.find('input').prop('checked', true);
+            }
+          }
+        });
+
+        if(menu.find('li').length == 0) {
+          menu.hide();
+        }
+      }
+    },
+
+    /**
+     *
+     * @param search
+     * @param callback
+     * @private
+     */
+    _search: function(search, callback) {
+      var matchType = this.matchType || 'fromStart';
+      var val = search.term.toLowerCase();
+      this._queryServer(val);
+
+      var result = this.result;
+      var list = [];
+
+      for(var p in result) if(result.hasOwnProperty(p)) {
+        var value = result[p][0];
+        if(value != null &&
+            (matchType === 'fromStart' && value.toLowerCase().indexOf(val) == 0) ||
+            (matchType === 'all' && value.toLowerCase().indexOf(val) > -1)) {
+          list.push(value);
+        }
+      }
+
+      callback(list);
+    },
+
+    /**
+     *
+     * @private
+     */
+    _endSearch: function() {
+      this.textbox.autocomplete("close");
     },
 
     /**
@@ -163,7 +306,7 @@ define(['./QueryComponent', './BaseComponent', '../lib/jquery', 'amd!../lib/jque
      * @private
      */
     _processAutoBoxChange : function() {
-      this.autoBoxOpt.processAutoBoxChange();
+      this.textbox.autocomplete("change");
     }
   });
 
