08103fcb9ab1eb6be1812ab2fa10a53f1efab615
Pedro Teixeira <pedrofvteixeira@gmail.com>
2015-06-29 11:35:04 +0100

    [CDA-154] Promote CoreBeanFactory to CPF 	- removed CDF's CoreBeanFactory / ICdfBeanFactory classes and started using CPF's 	- removed spring ivy dependencies ( less dependency footprint )

--- cdf-core/src/org/pentaho/cdf/environment/ICdfEnvironment.java
+++ cdf-core/src/org/pentaho/cdf/environment/ICdfEnvironment.java
@@ -4,19 +4,19 @@ import java.util.Locale;
 
 import org.pentaho.cdf.environment.broker.ICdfInterPluginBroker;
 import org.pentaho.cdf.environment.configurations.IHibernateConfigurations;
-import org.pentaho.cdf.environment.factory.ICdfBeanFactory;
 import org.pentaho.cdf.environment.packager.ICdfHeadersProvider;
 import org.pentaho.cdf.environment.paths.ICdfApiPathProvider;
 import org.pentaho.cdf.environment.templater.ITemplater;
 
 import pt.webdetails.cpf.PluginEnvironment;
+import pt.webdetails.cpf.bean.IBeanFactory;
 import pt.webdetails.cpf.exceptions.InitializationException;
 import pt.webdetails.cpf.repository.api.IContentAccessFactory;
 import pt.webdetails.cpf.resources.IResourceLoader;
 
 public interface ICdfEnvironment {
 
-  public void init( ICdfBeanFactory factory ) throws InitializationException;
+  public void init( IBeanFactory factory ) throws InitializationException;
 
   public void refresh();
 
