fa084c3f9e5805c7d7e736c67a7c9b1ebd566562
Diogo Mariano <dmariano@pentaho.com>
2015-09-28 18:35:07 +0100

    [CDF-655] - backport of BACKLOG-5349 to legacy js

--- cdf-core/test-js/legacy/components/input-spec-legacy.js
+++ cdf-core/test-js/legacy/components/input-spec-legacy.js
@@ -183,3 +183,166 @@ describe("The DateInput Component #", function() {
     }, 100);
   });
 });
+
+
+/**
+ * ## The Radio Component
+ */
+describe("The Radio Component #", function(){
+  var myDashboard = _.extend({}, Dashboards);
+  myDashboard.addParameter('region', "");
+
+  var radioComponent = window.RadioComponent = new RadioComponent();
+  $.extend(radioComponent, {
+    name: "radioComponent",
+    type: "radioComponent",
+    parameters:[],
+    path: "/fake/regions.xaction",
+    parameter: "region",
+    separator: ",&nbsp;",
+    valueAsId: true,
+    htmlObject: "sampleObject",
+    executeAtStart: true,
+    postChange: function() {
+      return "you chose: " + this.dashboard.getParameterValue(this.parameter);
+    }
+  });
+
+  myDashboard.addComponent(radioComponent);
+  /**
+   * ## The Radio Component # allows a dashboard to execute update
+   */
+  it("allows a dashboard to execute update", function(done) {
+    spyOn(radioComponent, 'update').and.callThrough();
+    spyOn($, "ajax").and.callFake(function() {
+      return {responseXML: "<test/>"};
+    });
+
+    // listen to cdf:postExecution event
+    radioComponent.once("cdf:postExecution", function() {
+      expect(radioComponent.update).toHaveBeenCalled();
+      done();
+    });
+
+    myDashboard.update(radioComponent);
+  });
+
+  /**
+   * ## The Radio Component # behaves correctly with parameter as null
+   */
+  it("behaves correctly with parameter as null", function(done) {
+    myDashboard.setParameter("region", null);
+    spyOn(radioComponent, 'update').and.callThrough();
+    spyOn($, "ajax").and.callFake(function() {
+      return {responseXML: "<test/>"};
+    });
+
+    // listen to cdf:postExecution event
+    radioComponent.once("cdf:postExecution", function() {
+      expect(radioComponent.update).toHaveBeenCalled();
+      done();
+    });
+
+    myDashboard.update(radioComponent);
+  });
+
+  /**
+   * ## The Radio Component # behaves correctly with parameter as undefined
+   */
+  it("behaves correctly with parameter as undefined", function(done) {
+    myDashboard.setParameter("region", undefined);
+    spyOn(radioComponent, 'update').and.callThrough();
+    spyOn($, "ajax").and.callFake(function() {
+      return {responseXML: "<test/>"};
+    });
+
+    // listen to cdf:postExecution event
+    radioComponent.once("cdf:postExecution", function() {
+      expect(radioComponent.update).toHaveBeenCalled();
+      done();
+    });
+
+    myDashboard.update(radioComponent);
+  });
+
+});
+
+/**
+ * ## The Checkbox Component
+ */
+describe("The Checkbox Component #", function(){
+  var myDashboard = _.extend({}, Dashboards);
+  myDashboard.addParameter('region', "");
+
+  var checkboxComponent = window.CheckComponent = new CheckComponent();
+  $.extend(checkboxComponent, {
+    name: "checkComponent",
+    type: "checkComponent",
+    parameters: [],
+    path: "/fake/test.xaction",
+    parameter: "region",
+    separator: ",&nbsp;",
+    valueAsId: true,
+    htmlObject: "sampleObject",
+    executeAtStart: true,
+    postChange: function() { return; }
+  });
+
+  myDashboard.addComponent(checkboxComponent);
+
+  /**
+   * ## The Checkbox Component # allows a dashboard to execute update
+   */
+  it("allows a dashboard to execute update", function(done) {
+    spyOn(checkboxComponent, 'update').and.callThrough();
+    spyOn($, "ajax").and.callFake(function() {
+      return {responseXML: "<test/>"};
+    });
+
+    // listen to cdf:postExecution event
+    checkboxComponent.once("cdf:postExecution", function() {
+      expect(checkboxComponent.update).toHaveBeenCalled();
+      done();
+    });
+
+    myDashboard.update(checkboxComponent);
+  });
+
+  /**
+   * ## The Checkbox Component # behaves correctly with parameter as null
+   */
+  it("behaves correctly with parameter as null", function(done) {
+    myDashboard.setParameter("region", null);
+    spyOn(checkboxComponent, 'update').and.callThrough();
+    spyOn($, "ajax").and.callFake(function() {
+      return {responseXML: "<test/>"};
+    });
+
+    // listen to cdf:postExecution event
+    checkboxComponent.once("cdf:postExecution", function() {
+      expect(checkboxComponent.update).toHaveBeenCalled();
+      done();
+    });
+
+    myDashboard.update(checkboxComponent);
+  });
+
+  /**
+   * ## The Checkbox Component # behaves correctly with parameter as undefined
+   */
+  it("behaves correctly with parameter as undefined", function(done) {
+    myDashboard.setParameter("region", undefined);
+    spyOn(checkboxComponent, 'update').and.callThrough();
+    spyOn($, "ajax").and.callFake(function() {
+      return {responseXML: "<test/>"};
+    });
+
+    // listen to cdf:postExecution event
+    checkboxComponent.once("cdf:postExecution", function() {
+      expect(checkboxComponent.update).toHaveBeenCalled();
+      done();
+    });
+
+    myDashboard.update(checkboxComponent);
+  })
+});
