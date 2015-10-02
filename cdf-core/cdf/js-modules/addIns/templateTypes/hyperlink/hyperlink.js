d23e3ac65594f54fc97764b5146bf833ec0fb23e
Miguel Gaspar <miguel.gaspar@webdetails.pt>
2015-10-02 14:19:20 +0100

    [CDF-637] - Adding 9 addins to the template component.

---
+++ cdf-core/cdf/js-modules/addIns/templateTypes/hyperlink/hyperlink.js
@@ -0,0 +1,58 @@
+define([
+       '../../../AddIn',
+        '../../../Dashboard',
+        '../../../dashboard/Utils',
+        '../../../Logger',
+        '../../../lib/jquery'
+    ],
+     function(AddIn, Dashboard, Utils, Logger, $, _) {
+        var hyperlink = {
+            name: "hyperlink",
+            label: "hyperlink",
+            defaults: {
+                openInNewTab: true,
+                prependHttpIfNeeded: true,
+                regexp: null,
+                pattern: null,
+                urlReference: 1,
+                labelReference: 0,
+                cssClass: 'hyperlinkContainer',
+                layout: '<div> {CONTENT} </div>'
+            },
+        
+            init: function() { },
+            
+            implementation: function(tgt, st, opt) {
+                var opts = $.extend(true, this.defaults, opt);
+                var $tgt = $(tgt);
+                var $html = $(opts.layout).addClass();
+                var link, 
+                    label;
+                if(opts.pattern) {
+                    var re = new RegExp(opts.pattern),
+                        results = [];
+                    st.value.replace(re, function(g0, g1){ results.push(g1);});
+                    link = results[opts.urlReference];
+                    label = results[opts.labelReference];
+                } else {
+                    link = st.value;
+                    label = st.value;
+                }
+                if(opts.prependHttpIfNeeded && !/^https?:\/\//.test(link)) {
+                    link = "http://" + link;
+                }
+                if(opts.regexp === null || (new RegExp(opts.regexp).test(st.value))) {
+                    var $a = $('<a></a>').attr("href", link).text(label);
+                    if(opts.openInNewTab) {
+                        $a.attr("target", "_blank");
+                    }
+                    $tgt.empty().append($html.html($a));
+                }
+            }
+        };
+        
+        Dashboard.registerGlobalAddIn("Template", "templateType", new AddIn(hyperlink));
+        
+        return hyperlink;
+    }
+);
