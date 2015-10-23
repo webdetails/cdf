94067e8102f32a60c6fbd0d39ee8e8cef42a44d1
João Velasques <joao.velasques@webdetails.pt>
2015-10-23 12:06:17 +0100

    [CDF-663] - Search Term will no longer be sent to the server as lowercase only

--- cdf-core/cdf/js-modules/components/filter/controllers/Manager.js
+++ cdf-core/cdf/js-modules/components/filter/controllers/Manager.js
@@ -372,7 +372,7 @@ define([
      */
     onFilterChange: function(text) {
       var filter, that;
-      this._searchPattern = text.trim().toLowerCase();
+      this._searchPattern = text.trim();
       filter = _.bind(function() {
         var isMatch;
         isMatch = this.filter(this._searchPattern, "", this.get('configuration').search.matcher);
