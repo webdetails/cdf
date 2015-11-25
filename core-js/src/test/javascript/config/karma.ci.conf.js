7efe3831dee2742f998273486e2ec7ddf768c30d
Diogo Mariano <dmariano@pentaho.com>
2015-11-25 12:48:12 +0000

    [CLEANUP] - Fixed console log errors for supported messages property files in karma tests of core requirejs modules - Removed unneeded package.json file

--- core-js/src/test/javascript/config/karma.ci.conf.js
+++ core-js/src/test/javascript/config/karma.ci.conf.js
@@ -36,9 +36,10 @@ module.exports = function(config) {
       'src/test/javascript/config/require-config.js',
       {pattern: 'src/test/javascript/cdf/**/*.ext.js', included: true},
       // fix 404 messages
-      {pattern: 'target/test-javascript/cdf/**/*.png', watched: false, included: false, served: true},
-      {pattern: 'target/test-javascript/cdf/**/*.gif', watched: false, included: false, served: true},
-      {pattern: 'target/test-javascript/cdf/**/*.svg', watched: false, included: false, served: true}
+      {pattern: 'src/test/javascript/cdf/dashboard/*.properties', watched: false, included: true, served: true},
+      {pattern: 'target/test-javascript/**/*.png', watched: false, included: false, served: true},
+      {pattern: 'target/test-javascript/**/*.gif', watched: false, included: false, served: true},
+      {pattern: 'target/test-javascript/**/*.svg', watched: false, included: false, served: true}
     ],
 
     preprocessors: {'src/test/javascript/cdf/**/*.js': 'coverage'},
@@ -76,7 +77,7 @@ module.exports = function(config) {
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_WARN,
+    logLevel: config.LOG_ERROR,
 
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
