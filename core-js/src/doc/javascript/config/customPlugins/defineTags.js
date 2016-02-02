d2161e8ca220f6454cf25eec176084d7952f07b0
Diogo Mariano <dmariano@pentaho.com>
2016-02-02 14:27:29 +0000

    [BACKLOG-6242] - Added new template with initial version of the stylisation based on MindTouch current api style

---
+++ core-js/src/doc/javascript/config/customPlugins/defineTags.js
@@ -0,0 +1,58 @@
+/**
+ * Define your custom tags inside this block
+ */
+exports.defineTags = function (dictionary) {
+  /**
+   * Tag options:
+   * 
+   * @property {boolean}  canHaveType              Default Value (false)
+   * @property {boolean}  canHaveName              Default Value (false)
+   * @property {boolean}  isNamespace              Default Value (false)
+   * @property {boolean}  mustHaveValue            Default Value (false)
+   * @property {boolean}  mustNotHaveDescription   Default Value (false)
+   * @property {boolean}  mustNotHaveValue         Default Value (false)
+   * @property {function} onTagged
+   */
+  dictionary.defineTag('amd', {
+
+    /**
+     * A callback function executed when the tag is found
+     * @param  {Object} doclet Doclet where the tag was "tagged"
+     * @param  {Object} tag    The tag data
+     */
+    onTagged: function (doclet, tag) {
+      doclet.amd = tag;
+    }
+  });
+
+  /**
+   * (Re)defining extends tag
+   */
+  dictionary.defineTag('extends', {
+    mustHaveValue: true,
+    // Allow augments value to be specified as a normal type, e.g. {Type}
+    //onTagText: parseTypeText,
+    onTagged: function(doclet, tag) {
+      var value = tag.value;
+      if(value.indexOf("@link") < 0) {
+        value = firstWordOf(value);
+      }
+      doclet.augment( value );
+    }
+  });
+
+  //TODO: Check what this is doing in the default tag
+  /*function parseTypeText(text) {
+    var tagType = jsdoc.tag.type.parse(text, false, true);
+    return tagType.typeExpression || text;
+  }*/
+
+  function firstWordOf(string) {
+    var m = /^(\S+)/.exec(string);
+    if (m) { 
+      return m[1]; 
+    } else { 
+      return '';
+    }
+  }
+};
