b42e96686671cabf603b540b4b798583a4a92471
Andre <andre.jorge@webdetails.pt>
2016-02-02 19:32:43 +0000

    [DOC] removed externals

--- core-js/src/main/javascript/cdf/components/ButtonComponent.js
+++ core-js/src/main/javascript/cdf/components/ButtonComponent.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright 2002 - 2015 Webdetails, a Pentaho company. All rights reserved.
+ * Copyright 2002 - 2016 Webdetails, a Pentaho company. All rights reserved.
  *
  * This software was developed by Webdetails and is provided under the terms
  * of the Mozilla Public License, Version 2.0, or any later version. You may not use
@@ -144,7 +144,8 @@ define([
     },
 
     /**
-     * Returns whether or not the button is a {@link external:jQueryUI|jQueryUI} button.
+     * Returns whether or not the button is a
+     * {@link http://api.jqueryui.com/button/|jQueryUI.button} widget.
      *
      * @private
      */
--- core-js/src/main/javascript/cdf/dashboard/Dashboard.notifications.js
+++ core-js/src/main/javascript/cdf/dashboard/Dashboard.notifications.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright 2002 - 2015 Webdetails, a Pentaho company. All rights reserved.
+ * Copyright 2002 - 2016 Webdetails, a Pentaho company. All rights reserved.
  *
  * This software was developed by Webdetails and is provided under the terms
  * of the Mozilla Public License, Version 2.0, or any later version. You may not use
@@ -55,7 +55,7 @@ define([
     },
 
     /**
-     * Sets additional {@link external:jQuery.blockUI|jQuery.blockUI} options based on the defaults already set.
+     * Sets additional {@link http://malsup.com/jquery/block/|jQuery.blockUI} options based on the defaults already set.
      *
      * @param {Object}  options             The options to configure.
      * @param {string}  options.message     The message or HTML to display in the UI overlay.
