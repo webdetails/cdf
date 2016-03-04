0f1e47fbd02b53191f3cbd2d023dfb6a7e125a7e
Elio Freitas <elio.freitas@webdetails.pt>
2016-03-04 14:23:26 +0000

    [CDF-796] - On the Filter Component with the sortByLabel enabled, and a page length defined: scrolling down to fetch more items yields an exception (backport to 5.x)

---
+++ cdf-core/cdf/js/components/filter/js/TreeFilter/views/scrollbar/MCustomScrollBarEngine.js
@@ -0,0 +1,40 @@
+/*!
+ * Copyright 2002 - 2016 Webdetails, a Pentaho company. All rights reserved.
+ *
+ * This software was developed by Webdetails and is provided under the terms
+ * of the Mozilla Public License, Version 2.0, or any later version. You may not use
+ * this file except in compliance with the license. If you need a copy of the license,
+ * please go to http://mozilla.org/MPL/2.0/. The Initial Developer is Webdetails.
+ *
+ * Software distributed under the Mozilla Public License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. Please refer to
+ * the license for the specific language governing your rights and limitations.
+ */
+/**
+ * @summary MCustomScrollBarEngineImplementation implementation of ScrollBarHandler
+ * @description MCustomScrollBarEngineImplementation implementation of ScrollBarHandler
+ */
+var MCustomScrollBarEngine;
+(function(AbstractScrollBarHandler, $){
+
+  MCustomScrollBarEngine = AbstractScrollBarHandler.extend({
+    scrollbar: null,
+    constructor: function (view) {
+      var options = $.extend(true, {}, view.config.view.scrollbar.options, {
+        callbacks: {
+          onTotalScroll: function () {
+            return view.trigger('scroll:reached:bottom', view.model);
+          },
+          onTotalScrollBack: function () {
+            return view.trigger('scroll:reached:top', view.model);
+          }
+        }
+      });
+      this.scrollbar = view.$(view.config.view.slots.children).parent().mCustomScrollbar(options);
+    },
+    scrollToPosition: function(position) {
+      this.scrollbar.mCustomScrollbar("scrollTo",position);
+    }
+  });
+})(AbstractScrollBarHandler, $);
+
