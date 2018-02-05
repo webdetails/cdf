cbbcbae5033b3554750630a3404e2a8b956bf4fe
Ricardo Araújo <raraujo@pentaho.com>
2018-02-05 11:15:57 +0000

    [BACKLOG-21176] added a new success handler to SolrQuery

--- core-js/src/main/javascript/cdf/queries/SolrQuery.js
+++ core-js/src/main/javascript/cdf/queries/SolrQuery.js
@@ -15,8 +15,9 @@ define([
     './BaseQuery',
     './SolrQuery.ext',
     '../dashboard/Dashboard.query',
-    '../Logger'
-], function(BaseQuery, SolrQueryExt, Dashboard, Logger) {
+    '../Logger',
+    'amd!../lib/lodash'
+], function(BaseQuery, SolrQueryExt, Dashboard, Logger,_) {
   "use strict";
 
   var SOLR_TYPE = "solr";
@@ -29,8 +30,12 @@ define([
     defaults: {
       endpoint: SolrQueryExt.getEndpoint(),
       collection: SolrQueryExt.getCollection(),
+      successCallback: function (data) {
+        console.log("i'm here: ",data);
+      },
       ajaxOptions: {
         async: true,
+        type: "GET",
         xhrFields: {
           withCredentials: true
         }
@@ -43,14 +48,17 @@ define([
     init: function(options) {
       Logger.log("Apache Solr - Initializing Apache Solr Query");
 
-      var endpoint = options.endpoint != null ? options.endpoint : this.getOption("endpoint");
-      var collection = options.collection != null ? options.collection : this.getOption("collection");
+      var endpoint = options.endpoint !== null ? options.endpoint : this.getOption("endpoint");
+      var collection = options.collection !== null ? options.collection : this.getOption("collection");
+      var requestHandler = options.requestHandler !== null ? options.requestHandler : this.getOption("requestHandler");
+      var responseType = options.responseType !== null ? options.responseType : this.getOption("responseType");
+      var solrQuery = options.solrQuery !== null ? options.solrQuery : this.getOption("solrQuery");
 
       if ( !endpoint.endsWith('/') ) {
         endpoint = endpoint + "/";
       }
 
-      this.setOption( 'url', endpoint + collection );
+      this.setOption( 'url', endpoint + collection + '/' + requestHandler + '?q=' + solrQuery + '&wt=' + responseType);
     },
 
     /** @override */
@@ -60,8 +68,79 @@ define([
       return {
         url: this.getOption('url')
       };
+    },
+
+    /** @override */
+    getSuccessHandler: function(callback) {
+
+      var baseSuccessCallback = this.base(callback);
+
+      return function(json) {
+
+        // Transform into CDA format
+        var jsonObjects = JSON.parse(json);
+        var soltResponse = jsonObjects.response;
+        var docs = soltResponse.docs;
+
+        /*
+         *  TODO: This is a mock schema, replace with real schema from CDE editor
+         **/
+        var cdeSchema = {
+          "columnNames":[
+            "Winner",
+            "WRank",
+            "nested"
+          ],
+          "columnTypes":[
+            "string",
+            "int",
+            "int"
+          ],
+          "columnPaths":[
+            "Winner",
+            "WRank",
+            "nested.element.very.deep"
+          ]
+        }
+
+
+        var cdaData = {
+          "resultset":[],
+          "metadata":[]
+        }
+
+        // Get Metadata
+        var colIndex = 0;
+        cdeSchema.columnNames.forEach(function(elem, index){
+          var metadataRow = {
+            "colName": elem,
+            "colType": cdeSchema.columnTypes[index],
+            "colIndex": colIndex++
+          }
+          cdaData.metadata.push(metadataRow);
+        });
+
+        // Get Resultset
+        docs.forEach(function(docElem, docIndex){
+          var docRow = [];
+          cdeSchema.columnPaths.forEach(function(keyElem, keyIndex){
+            var elem = docElem[keyElem];
+            //deal with nesting
+            if(elem === undefined){
+              elem = _.get(docElem, keyElem);
+            }
+            docRow.push(Array.isArray(elem) ? elem[0] : elem)
+          });
+          cdaData.resultset.push(docRow);
+        });
+
+
+        // console.log(cdaData); // TODO: Delete, was used for debugging
+        baseSuccessCallback(cdaData);
+      };
     }
   };
+
 
   Dashboard.registerGlobalQuery(SOLR_TYPE, solrQueryOpts);
 });
