74a6be0b4992b5e9dc4d7d88f4a5206f7a53073a
Denis Protopopov <Denis_Protopopov@epam.com>
2018-03-01 11:58:40 +0400

    [CDE-943]-Table component that has click action, whenever gets an empty resultset throws an error in jquery.dataTables file.
      - fixed
      - apply fix to legacy version
      - updated copyright year

--- pentaho-js/src/main/javascript/cdf/components/TableComponent.js
+++ pentaho-js/src/main/javascript/cdf/components/TableComponent.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright 2002 - 2017 Webdetails, a Hitachi Vantara company. All rights reserved.
+ * Copyright 2002 - 2018 Webdetails, a Hitachi Vantara company. All rights reserved.
  *
  * This software was developed by Webdetails and is provided under the terms
  * of the Mozilla Public License, Version 2.0, or any later version. You may not use
@@ -598,6 +598,10 @@ define([
             return;
           } else if(target.get(0).tagName != 'TD') {
             target = target.closest('td');
+          }
+          if(!myself.dataTable.api(true).cell(target.get(0)).index()) {
+            Logger.warn("Click on invalid data detected.");
+            return;
           }
           var position = myself.dataTable.fnGetPosition(target.get(0));
           state.rawData = myself.rawData;
