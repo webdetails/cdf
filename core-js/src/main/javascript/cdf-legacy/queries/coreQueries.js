fc0e6e4fd58c9b479d49d9e5576390a70f9ed21d
Pedro Vale <pedro.vale@hitachivantara.com>
2018-03-23 16:12:11 +0000

    [CLEANUP] - Removed usage of undeclared global variable

--- core-js/src/main/javascript/cdf-legacy/queries/coreQueries.js
+++ core-js/src/main/javascript/cdf-legacy/queries/coreQueries.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright 2002 - 2017 Webdetails, a Hitachi Vantara company. All rights reserved.
+ * Copyright 2002 - 2018 Webdetails, a Hitachi Vantara company. All rights reserved.
  *
  * This software was developed by Webdetails and is provided under the terms
  * of the Mozilla Public License, Version 2.0, or any later version. You may not use
@@ -281,7 +281,7 @@
       }
     },
 
-    // Gets the previous _pageSize results
+    // Gets the previous pageSize results
     previousPage: function(outsideCallback) {
       var page = this.getOption('page'),
           pageSize = this.getOption('pageSize');
