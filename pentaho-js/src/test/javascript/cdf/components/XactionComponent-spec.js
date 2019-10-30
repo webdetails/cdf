93886a733540eb56628dac05d11e2ab7a22007b0
Bruno Costa <bruno.costa@hitachivantara.com>
2019-10-30 10:00:00 +0000

    [SP-5292] - Backport of PDB-2060 - Xactions cannot be used in Dashboard Designer (8.3 Suite)

--- pentaho-js/src/test/javascript/cdf/components/XactionComponent-spec.js
+++ pentaho-js/src/test/javascript/cdf/components/XactionComponent-spec.js
@@ -14,8 +14,26 @@
 define([
   "cdf/Dashboard.Clean",
   "cdf/components/XactionComponent",
+  "cdf/components/XactionComponent.ext",
   "cdf/lib/jquery"
-], function(Dashboard, XactionComponent, $) {
+], function(Dashboard, XactionComponent, XactionComponentExt, $) {
+
+  function createDashboard( xActionComponent ) {
+    var dashboard = new Dashboard();
+    dashboard.init();
+    dashboard.addComponent(xActionComponent);
+    return dashboard;
+  }
+
+  function createXActionComponent( action, path, solution) {
+    return new XactionComponent({
+      name: "xActionComponent",
+      action: action,
+      path: path,
+      solution: solution,
+      iframe: "mock_iframe"
+    });
+  }
 
   /**
    * ## The Xaction Component
@@ -56,5 +74,74 @@ define([
 
       dashboard.update(xactionComponent);
     });
+
   });
+
+  describe( "should call getCdfXAction method with correct parameters", function () {
+
+    it("should call getCdfXAction method with action null", function(done) {
+      var action = null;
+      var path = "/public/mock/";
+      var solution = "";
+      var xActionComponent = createXActionComponent( action, path, solution );
+      var dashboard = createDashboard( xActionComponent );
+
+      spyOn(xActionComponent, "update").and.callThrough();
+      spyOn(XactionComponentExt, "getCdfXaction").and.callThrough();
+
+      // listen to cdf:postExecution event
+      xActionComponent.once("cdf:postExecution", function() {
+        expect(xActionComponent.update).toHaveBeenCalled();
+        expect(XactionComponentExt.getCdfXaction)
+            .toHaveBeenCalledWith(path, "", solution);
+        done();
+      });
+
+      dashboard.update(xActionComponent);
+    });
+
+    it("should call getCdfXAction method with action in path", function(done) {
+      var action = "action.xaction";
+      var path = "/public/mock/action.xaction";
+      var solution = "";
+      var xActionComponent = createXActionComponent( action, path, solution );
+      var dashboard = createDashboard( xActionComponent );
+
+      spyOn(xActionComponent, "update").and.callThrough();
+      spyOn(XactionComponentExt, "getCdfXaction").and.callThrough();
+
+      // listen to cdf:postExecution event
+      xActionComponent.once("cdf:postExecution", function() {
+        expect(xActionComponent.update).toHaveBeenCalled();
+        expect(XactionComponentExt.getCdfXaction)
+            .toHaveBeenCalledWith(path, "", solution);
+        done();
+      });
+
+      dashboard.update(xActionComponent);
+    });
+
+    it("should call getCdfXAction method without action in path", function(done) {
+      var action = "action.xaction";
+      var path = "/public/mock/";
+      var solution = "";
+      var xActionComponent = createXActionComponent( action, path, solution );
+      var dashboard = createDashboard( xActionComponent );
+
+      spyOn(xActionComponent, "update").and.callThrough();
+      spyOn(XactionComponentExt, "getCdfXaction").and.callThrough();
+
+      // listen to cdf:postExecution event
+      xActionComponent.once("cdf:postExecution", function() {
+        expect(xActionComponent.update).toHaveBeenCalled();
+        expect(XactionComponentExt.getCdfXaction)
+            .toHaveBeenCalledWith(path, action, solution);
+        done();
+      });
+
+      dashboard.update(xActionComponent);
+    });
+
+  });
+
 });
