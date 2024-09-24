151e10cb73b023346bee3685a4a03c1006204edc
lgrill-pentaho <larry.grill@hitachivantara.com>
2024-09-24 18:24:18 -0400

    Revert "HNC-766: updated license and copyright headers"
    This reverts commit 917c3db72ffaaa5a86512963409c8c0a55a91255.

--- core-js/src/main/javascript/lib/mustacheWax/mustache-wax.js
+++ core-js/src/main/javascript/lib/mustacheWax/mustache-wax.js
@@ -1,14 +1,3 @@
-/*! ******************************************************************************
- *
- * Pentaho
- *
- * Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
- *
- * Use of this software is governed by the Business Source License included
- * in the LICENSE.TXT file.
- *
- * Change Date: 2028-08-13
- ******************************************************************************/
 (function(root, wax) {
 	// Set up Backbone appropriately for the environment. Start with AMD.
 	if (typeof define === 'function' && define.amd) {
