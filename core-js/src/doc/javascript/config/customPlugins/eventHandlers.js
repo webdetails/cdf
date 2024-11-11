8b964246723e2b36f9d6e7531ae9f22de03e7b7e
lgrill-pentaho <larry.grill@hitachivantara.com>
2024-11-11 13:50:58 -0500

    DEVO-11194: updated license and copyright headers

--- core-js/src/doc/javascript/config/customPlugins/eventHandlers.js
+++ core-js/src/doc/javascript/config/customPlugins/eventHandlers.js
@@ -1,3 +1,14 @@
+/*! ******************************************************************************
+ *
+ * Pentaho
+ *
+ * Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file.
+ *
+ * Change Date: 2029-07-20
+ ******************************************************************************/
 /**
  * Event Handlers {@link http://usejsdoc.org/about-plugins.html#event-handlers}
  *
